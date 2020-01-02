1f72cb38fb3129d5a27d540e39a710ce24bf1c44
dansebmey <dansebmey@gmail.com>
2020-01-02 10:26:32 +0100

    Cleanup unused imports

--- src/main/java/nl/nn/testtool/TestTool.java
+++ src/main/java/nl/nn/testtool/TestTool.java
@@ -17,13 +17,10 @@ package nl.nn.testtool;
 
 import java.rmi.server.UID;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
