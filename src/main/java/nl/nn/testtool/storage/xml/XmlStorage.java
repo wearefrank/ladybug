6d0ce16b6142e92173a3ec44a0b4f390fc21992d
Murat Kaan <muratkaanmeral@gmail.com>
2020-05-01 17:41:25 +0200

    Extend XmlStorage to be used as test storage for ladybug.

--- src/main/java/nl/nn/testtool/storage/xml/XmlStorage.java
+++ src/main/java/nl/nn/testtool/storage/xml/XmlStorage.java
@@ -1,6 +1,7 @@
 package nl.nn.testtool.storage.xml;
 
 import nl.nn.testtool.Report;
+import nl.nn.testtool.storage.CrudStorage;
 import nl.nn.testtool.storage.LogStorage;
 import nl.nn.testtool.storage.StorageException;
 import nl.nn.testtool.util.LogUtil;
@@ -19,50 +20,140 @@ import java.util.List;
  * Handles report storage for ladybug.
  * Stores reports in xml format.
  */
-public class XmlStorage implements LogStorage {
-	private String name;
-	String metadataFile;
+public class XmlStorage implements LogStorage, CrudStorage {
+	private String name, metadataFile, reportsFolderPath, stage;
 	private MetadataHandler metadataHandler;
 	private File reportsFolder;
 	Logger logger = LogUtil.getLogger(XmlStorage.class);
 
 	/**
 	 * Initializes the storage. Creating necessary folders and metadata file.
+	 *
 	 * @throws Exception Security exception that might be thrown by JVM.
 	 */
 	public void init() throws Exception {
-		if (!reportsFolder.exists() && !reportsFolder.mkdirs()) {
-			throw new Exception("Could not create reports folder!");
-		}
-
+		getReportsFolder();
 		if (StringUtils.isEmpty(metadataFile)) {
 			metadataFile = new File(reportsFolder, "metadata.xml").getAbsolutePath();
 			logger.warn("Metadatafile was not set. Using " + metadataFile);
 		}
-		metadataHandler = new MetadataHandler(metadataFile);
+		metadataHandler = new MetadataHandler(metadataFile, this, false);
+	}
+
+	/**
+	 * Stores the given report in the given file.
+	 * If file does not exist, it creates a new one.
+	 * Otherwise, it overwrites the contents of the file.
+	 *
+	 * @param report Report to be written.
+	 * @param path   File to be written.
+	 * @throws StorageException
+	 */
+	private void store(Report report, String path) throws StorageException {
+		try {
+			File file = new File(path);
+			if (!file.exists()) {
+				file.getParentFile().mkdirs();
+				file.createNewFile();
+			}
+
+			FileWriter writer = new FileWriter(file, false);
+			String xml = report.toXml(null, false, true);
+			writer.write(xml);
+			writer.close();
+		} catch (Exception e) {
+			throw new StorageException("Could not write report [" + report.getCorrelationId() + "] to [" + path + "].", e);
+		}
 	}
 
 	@Override
 	public void storeWithoutException(Report report) {
 		try {
+			store(report);
+		} catch (StorageException e) {
+			logger.error("Error while writing the report!", e);
+		}
+	}
+
+	@Override
+	public void store(Report report) throws StorageException {
+		try {
 			// Make sure we are not overriding any previous report
 			// that might have been handled by another metadatahandler file.
-			long storageid;
+			Metadata metadata = metadataHandler.getMetadata(report.getCorrelationId());
 			File reportFile;
-			do {
-				storageid = metadataHandler.getNextStorageId();
-				reportFile = new File(reportsFolder, storageid + ".xml");
-			} while (reportFile.isFile());
+			if (metadata == null) {
+				int storageid;
+				File parentFolder = (report.getPath() != null) ? new File(reportsFolder, report.getPath()) : reportsFolder;
+				do {
+					storageid = metadataHandler.getNextStorageId();
+					reportFile = new File(parentFolder, storageid + ".xml");
+				} while (reportFile.isFile());
 
-			Metadata metadata = Metadata.fromReport(report, storageid);
-			reportFile.createNewFile();
-
-			FileWriter writer = new FileWriter(reportFile, false);
-			writer.write(report.toXml());
-			writer.close();
+				metadata = Metadata.fromReport(report, storageid);
+			} else {
+				reportFile = new File(resolvePath(report.getCorrelationId()));
+			}
+			store(report, reportFile.getPath());
 			metadataHandler.add(metadata);
+
+		} catch (IOException e) {
+			throw new StorageException("Error while writing the report!", e);
+		}
+	}
+
+	@Override
+	public Report getReport(Integer storageId) throws StorageException {
+		Metadata m = metadataHandler.getMetadata(storageId);
+		String path = resolvePath(m.correlationId);
+		if (StringUtils.isEmpty(path))
+			throw new StorageException("Could not resolve path of report.");
+
+		File reportFile = new File(path);
+		if (!reportFile.isFile()) {
+			logger.error("Report with given storage id does not exits!");
+			throw new StorageException("Report with given storage id does not exits!");
+		}
+		try {
+			Report report = Report.fromXml(new String(Files.readAllBytes(Paths.get(reportFile.getAbsolutePath()))));
+			report.setStorage(this);
+			return report;
 		} catch (Exception e) {
-			logger.error("Error while writing the report!", e);
+			String err = "Could not read the report file [" + reportFile.getPath() + "]";
+			logger.error(err, e);
+			throw new StorageException(err, e);
+		}
+	}
+
+	@Override
+	public void update(Report report) throws StorageException {
+		try {
+			String oldpath = resolvePath(report.getCorrelationId());
+			metadataHandler.update(report);
+
+			store(report, resolvePath(report.getCorrelationId()));
+			if (StringUtils.isNotEmpty(oldpath))
+				new File(oldpath).delete();
+		} catch (IOException e) {
+			throw new StorageException("Error during updating report.", e);
+		}
+	}
+
+	@Override
+	public void delete(Report report) throws StorageException {
+		try {
+			metadataHandler.delete(report);
+
+			String path = resolvePath(report.getCorrelationId());
+			if (path == null) {
+				logger.warn("Could not find report file for report [" + report.getCorrelationId() + "]");
+				return;
+			}
+
+			if (!new File(path).delete())
+				throw new StorageException("Could not delete repot [" + report.getCorrelationId() + "] at [" + path + "]");
+		} catch (IOException e) {
+			throw new StorageException("Error while deleting the report [" + report.getCorrelationId() + "]", e);
 		}
 	}
 
@@ -99,7 +190,7 @@ public class XmlStorage implements LogStorage {
 	@Override
 	public List getStorageIds(String path) throws StorageException {
 		try {
-			MetadataHandler handler = new MetadataHandler(path);
+			MetadataHandler handler = new MetadataHandler(path, this, false);
 			return handler.getStorageIds();
 		} catch (IOException e) {
 			logger.error("Exception while trying to create metadatahandler.", e);
@@ -108,25 +199,7 @@ public class XmlStorage implements LogStorage {
 	}
 
 	@Override
-	public Report getReport(Integer storageId) throws StorageException {
-		File reportFile = new File(reportsFolder, storageId + ".xml");
-		if(!reportFile.isFile()) {
-			logger.error("Report with given storage id does not exits!");
-			throw new StorageException("Report with given storage id does not exits!");
-		}
-		try {
-			Report report =  Report.fromXml(new String(Files.readAllBytes(Paths.get(reportFile.getAbsolutePath()))));
-			report.setStorage(this);
-			return report;
-		} catch (Exception e) {
-			logger.error("Could not read the report file [" + reportFile.getName() + "]", e);
-			throw new StorageException("Could not read the report file [" + reportFile.getName() + "]", e);
-		}
-	}
-
-	@Override
 	public void close() {
-
 	}
 
 	@Override
@@ -144,11 +217,66 @@ public class XmlStorage implements LogStorage {
 		return SearchUtil.getUserHelp();
 	}
 
+	/**
+	 * Resolves and returns reports folder based on reportsFolderPath, and dtap.stage
+	 * @return reports folder
+	 * @throws StorageException If folder does not exist and we can not create.
+	 */
+	private File getReportsFolder() throws StorageException {
+		if (reportsFolder != null)
+			return reportsFolder;
+
+		if (StringUtils.isEmpty(reportsFolderPath)) {
+			// TODO: (For Jaco) which paths would you like to use as default?
+			if (stage != null && stage.equalsIgnoreCase("LOC")) {
+				reportsFolderPath = "ladybug-storage";
+			} else {
+				reportsFolderPath = "ladybug-storage";
+			}
+		}
+
+		reportsFolder = new File(reportsFolderPath);
+		if (!reportsFolder.exists() && !reportsFolder.mkdirs())
+			throw new StorageException("Could not create reports folder!");
+
+		return reportsFolder;
+	}
+
+	/**
+	 * Resolves the path of the report with given correlation Id
+	 * @param correlationId Correlation Id of the report to be resolved.
+	 * @return Path of the report. If report is not in metadata, null.
+	 */
+	private String resolvePath(String correlationId) {
+		Metadata metadata = metadataHandler.getMetadata(correlationId);
+		if (metadata == null)
+			return null;
+
+		File parentFolder = reportsFolder;
+		if (StringUtils.isNotEmpty(metadata.path) && !metadata.path.equalsIgnoreCase("null"))
+			parentFolder = new File(reportsFolder, metadata.path);
+
+		return new File(parentFolder, metadata.storageId + ".xml").getPath();
+	}
+
 	public void setReportsFolder(String reportsFolder) {
-		this.reportsFolder = new File(reportsFolder);
+		reportsFolderPath = reportsFolder;
 	}
 
 	public void setMetadataFile(String metadataFile) {
 		this.metadataFile = metadataFile;
+	}
+
+	/**
+	 * Resets the metadata handler by forcing it to build file from searching through directory.
+	 */
+	public void resetMetadataFile() {
+		MetadataHandler old = metadataHandler;
+		try {
+			metadataHandler = new MetadataHandler(metadataFile, this, true);
+		} catch (IOException e) {
+			logger.error("Error during metadata rebuilding. Reverting to the old version.", e);
+			metadataHandler = old;
+		}
 	}
 }
