28605f784541e9351ca9b939c3304ffecac9e3d1
Murat Kaan <muratkaanmeral@gmail.com>
2020-10-09 16:45:48 +0200

    Add more functionality and style to test tab buttons

--- src/main/resources/webapp/app/view1/view1.js
+++ src/main/resources/webapp/app/view1/view1.js
@@ -15,16 +15,33 @@ angular.module('myApp.view1', ['ngRoute'])
         $scope.storage = "logStorage";
         $scope.treeData = [];
         $scope.reports = [];
-        $scope.reportDetails = {text: "INIT", values: {"init": "a", "b": "extra data"}};
+        $scope.reportDetails = {
+            text: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<email>\n" +
+                "\t<recipients>\n" +
+                "\t\t<recipient type=\"to\">No scenarios found\n" +
+                "</recipient>\n" +
+                "\t</recipients>\n" +
+                "\t<from>LAPTOP-L30O5T9</from>\n" +
+                "\t<subject>SUCCESS Larva run results on LAPTOP-L30O5T9 (No scenarios found)</subject>\n" +
+                "\t<message>No scenarios found\n" +
+                "  </message>\n" +
+                "</email>", values: {"init": "a", "b": "extra data"}
+        };
+        $scope.limit = 5;
+        $scope.filters = {"limit": $scope.limit};
+
+        $scope.updateFilter = function (key, value) {
+            $scope.filters[key] = value;
+            $scope.updateTable();
+        }
 
         /*
         * Sends a GET request to the server and updates the tables ($scope.metadatas)
         */
-        $scope.updateTable = function (data = {}, limit = 10) {
-            if (!("limit" in data)) {
-                data["limit"] = limit;
-            }
-            $http.get($scope.apiUrl + "/metadata/" + $scope.storage, {params: data}).then(function (response) {
+        $scope.updateTable = function () {
+            console.log($scope.filters);
+            $http.get($scope.apiUrl + "/metadata/" + $scope.storage, {params: $scope.filters}).then(function (response) {
                 let fields = response.data["fields"];
                 let values = response.data["values"];
                 $scope.metadatas = [];
@@ -51,6 +68,11 @@ angular.module('myApp.view1', ['ngRoute'])
         $scope.refresh = function () {
             $http.get($scope.apiUrl + '/metadata').then(function (response) {
                 $scope.columns = response.data;
+                $scope.columns.forEach(function (element) {
+                    if ($scope.filters[element] === undefined) {
+                        $scope.filters[element] = "";
+                    }
+                });
                 console.log(response);
                 $scope.updateTable();
             }, function (response) {
@@ -62,12 +84,11 @@ angular.module('myApp.view1', ['ngRoute'])
          * Displays a selected reports in the content pane.
          */
         $scope.display_report = function (event, node) {
-            // Todo: display on the other column.
-            console.log("EVENT SELECTED " + node.nodeId);
-            console.log(event);
-            console.log(node);
             let ladybugData = node["ladybug"];
-
+            $('#code-wrapper').remove();
+            $('#details-row').after('<pre id="code-wrapper" class="prettify"><code id="code" class="lang-xml"></code></pre>');
+            $('#code').text(ladybugData["message"]);
+            console.log(ladybugData["message"]);
             $scope.reportDetails = {
                 text: ladybugData["message"],
                 values: {
@@ -78,11 +99,25 @@ angular.module('myApp.view1', ['ngRoute'])
                     "Checkpoint UID:": ladybugData["uid"],
                     "Number of characters:": "???",
                     "EstimatedMemoryUsage:": ladybugData["estimatedMemoryUsage"]
-                }
+                },
+                nodeId: node.nodeId
             };
-            console.log($scope.reportDetails);
             $scope.$apply();
+            PR.prettyPrint()
         };
+
+        $scope.updateTree = function () {
+            $('#tree').treeview({
+                data: $scope.treeData,
+                onNodeSelected: $scope.display_report,
+                onNodeUnselected: function (event, node) {
+                    console.log("UNSELECTED");
+                    console.log(event);
+                    console.log(node);
+                    $scope.reportDetails = {text: "", values: {}};
+                }
+            });
+        }
 
         $scope.remove_circulars = function (node) {
             if ("parent" in node) {
@@ -95,15 +130,15 @@ angular.module('myApp.view1', ['ngRoute'])
             }
         };
 
-        $scope.selectReport = function (report) {
+        $scope.selectReport = function (metadata) {
             console.log("CLICK");
-            console.log(report);
-            $http.get($scope.apiUrl + "/report/" + $scope.storage + "/" + report["storageId"])
+            console.log(metadata);
+            $http.get($scope.apiUrl + "/report/" + $scope.storage + "/" + metadata["storageId"])
                 .then(function (response) {
                     console.log(response);
                     console.log(response.data);
                     $scope.reports[response.data["storageId"]] = response.data;
-                    let report = {text: response.data["name"], icon: "fa fa-file-code", nodes: []};
+                    let report = {text: response.data["name"], ladybug: response.data,icon: "fa fa-file-code", nodes: []};
                     let checkpoints = response.data["checkpoints"];
                     let nodes = report.nodes;
                     let previous_node = null;
@@ -147,35 +182,139 @@ angular.module('myApp.view1', ['ngRoute'])
                     $scope.treeData.push(report);
                     console.log($scope.treeData);
                     // Update tree
-                    $('#tree').treeview({
-                        data: $scope.treeData,
-                        onNodeSelected: $scope.display_report,
-                        onNodeUnselected: function (event, node) {
-                            console.log("UNSELECTED");
-                            console.log(event);
-                            console.log(node);
-                            $scope.reportDetails = {text: "", values: {}};
-                        }
-                    });
+                    $scope.updateTree();
                 }, function (response) {
                     console.error(response);
                 });
 
         };
-        $scope.closeAll = function() {
+
+        $scope.openAll = function () {
+            $scope.metadatas.forEach(function (element) {
+                $scope.selectReport(element);
+            });
+        };
+
+        $scope.closeAll = function () {
             $scope.treeData = [];
             $('#tree').treeview({data: []});
             $scope.$apply();
         };
 
         $scope.expandAll = function () {
-            $('#tree').treeview('expandAll', { levels: 99, silent: true });
+            $('#tree').treeview('expandAll', {levels: 99, silent: true});
         }
 
         $scope.collapseAll = function () {
-            $('#tree').treeview('collapseAll', { silent: true });
+            $('#tree').treeview('collapseAll', {silent: true});
         }
 
+        $scope.logg = function () {
+            console.log($scope.filters);
+        }
+
+        $scope.toggleEdit = function () {
+            console.log("toggling");
+            let codeWrappers = $('#code-wrapper');
+            let htmlText = '<pre id="code-wrapper" class="prettify"><code id="code" class="lang-xml"></code></pre>';
+            let buttonText = 'Edit';
+
+            if (codeWrappers.get().length === 0 || codeWrappers.get()[0].tagName === "PRE") {
+                let rows = Math.min(20, $scope.reportDetails.text.split('\n').length);
+                htmlText = '<div id="code-wrapper" class=\"form-group\"><textarea class=\"form-control\" id=\"code\" rows="' + rows + '"></textarea></div>';
+                buttonText = 'Save';
+            }
+            // TODO: Save the text|
+            codeWrappers.remove();
+            $('#details-edit').text(buttonText);
+            $('#details-row').after(htmlText);
+            $('#code').text($scope.reportDetails.text);
+        }
+
+        $scope.getRootNode = function () {
+                let tree = $('#tree').treeview(true);
+                if (tree.getSelected().length === 0)
+                    return;
+                let node = tree.getSelected()[0];
+                while (node.parentId !== undefined) {
+                    node = tree.getNode(node.parentId);
+                }
+                return node;
+        }
+        // TODO: Move
+        // $scope.deleteSelected = function () {
+        //     let tree = $('#tree').treeview(true);
+        //     if (tree.getSelected().length === 0)
+        //         return;
+        //     let node = tree.getSelected()[0];
+        //     while (node.parentId !== undefined) {
+        //         node = tree.getNode(node.parentId);
+        //     }
+        //     console.log("DELETE");
+        //     console.log(node["ladybug"]["storageId"]);
+        //     $http.delete($scope.apiUrl + "/report/" + $scope.storage + "/" + node["ladybug"]["storageId"])
+        //         .then(function () {
+        //             tree.remove(node);
+        //         }, function (response) {
+        //             console.log(response);
+        //             alert(response.data);
+        //         });
+        // }
+
+        $scope.collapse = function () {
+            $('#tree').treeview('collapseNode', $scope.reportDetails.nodeId);
+        }
+        $scope.expand = function () {
+            let tree = $('#tree').treeview(true);
+            let node = tree.getNode($scope.reportDetails.nodeId);
+            let path = [];
+            path.push(node.nodeId);
+            while (node.parentId !== undefined) {
+                node = tree.getNode(node.parentId);
+                path.push(node.nodeId);
+            }
+            console.log("Expanding");
+            console.log(path);
+            for(let i = path.length-1; i >= 0; i--) {
+                tree.expandNode(path[i]);
+            }
+        }
+
+        $scope.rerun = function() {
+            let rootNode = $scope.getRootNode();
+            let data = {};
+            data[$scope.storage] = [rootNode["ladybug"]["storageId"]];
+            console.log("RERUN");
+            console.log(data);
+            $http.post($scope.apiUrl + "/runner/run/" + $scope.storage, data)
+                .then(function (response) {
+                    console.log(response);
+                }, function (response) {
+                    console.log(response);
+                });
+        };
+
+        $scope.download = function () {
+            let storageId = $scope.getRootNode()["ladybug"]["storageId"];
+            window.open($scope.apiUrl + "/report/download/" + $scope.storage + "/" + storageId);
+        }
+
+        $scope.copy = function (to) {
+            let data = {};
+            data[$scope.storage] = [$scope.getRootNode()["ladybug"]["storageId"]];
+            $http.put($scope.apiUrl + "/report/store/" + to, data);
+        }
+
+        $scope.close = function () {
+            let storageId = $scope.getRootNode()["ladybug"]["storageId"];
+            for (let i = 0; i < $scope.treeData.length; i ++) {
+                if (storageId === $scope.treeData[i]["ladybug"]["storageId"]) {
+                    $scope.treeData.splice(i, 1);
+                    i--;
+                }
+            }
+            $scope.updateTree();
+        }
 
         $scope.refresh();
     }]);
