252152772041391c02671f13961ff0466aaacd59
Murat Kaan <muratkaanmeral@gmail.com>
2020-10-23 16:33:52 +0200

    Add cors filter for development

---
+++ src/main/java/nl/nn/testtool/web/api/CorsFilter.java
@@ -0,0 +1,25 @@
+package nl.nn.testtool.web.api;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+public class CorsFilter implements ContainerResponseFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext,
+					   ContainerResponseContext responseContext) throws IOException {
+		System.err.println("FILTER IS RUNNING FROM LADYBUG!!!!");
+		responseContext.getHeaders().add(
+				"Access-Control-Allow-Origin", "*");
+		responseContext.getHeaders().add(
+				"Access-Control-Allow-Credentials", "true");
+		responseContext.getHeaders().add(
+				"Access-Control-Allow-Headers",
+				"origin, content-type, accept, authorization");
+		responseContext.getHeaders().add(
+				"Access-Control-Allow-Methods",
+				"GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	}
+}
