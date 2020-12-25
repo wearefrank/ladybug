cf91f96019a4cba6c2192c86bdc81a09bd5e3efb
Murat Kaan Meral <muratkaanmeral@gmail.com>
2020-12-25 15:46:43 +0100

    Add display component and refactor.

--- src/main/resources/webapp/app/views/debug/view1.js
+++ src/main/resources/webapp/app/views/debug/view1.js
@@ -13,8 +13,8 @@ angular.module('myApp.view1', ['ngRoute'])
         $scope.apiUrl = "http://localhost:8080/ibis_adapterframework_test_war_exploded/ladybug";
         $scope.columns = ['storageId', 'endTime', 'duration', 'name', 'correlationId', 'status', 'nrChpts', 'estMemUsage', 'storageSize'];
         $scope.storage = "debugStorage";
-        $scope.reportDetails = {text: "", values: {}};
         $scope.table2tree = {};
+        $scope.tree2display = {};
         $scope.limit = 5;
         $scope.filters = {"limit": $scope.limit};
         $scope.options = {
@@ -27,8 +27,6 @@ angular.module('myApp.view1', ['ngRoute'])
             "openLatest": 10,
             "openInProgress": 1
         };
-        $scope.availableStorages = {'runStorage': 'Test'};
-        $scope.stubStrategySelect = "";
 
         $scope.updateOptions = function() {
             $http.get($scope.apiUrl + "/testtool")
@@ -58,71 +56,6 @@ angular.module('myApp.view1', ['ngRoute'])
                 "regexFilter": $scope.options["regexFilter"]});
             $http.post($scope.apiUrl + "/testtool/transformation", {"transformation": $scope.options['transformation']});
         }
-
-        /*
-         * Displays a selected reports in the content pane.
-         */
-        $scope.display_report = function (rootNode, event, node) {
-            $scope.selectedNode = rootNode;
-            if (node === null) {
-                $scope.reportDetails = {text: "", values: {}};
-                return;
-            }
-            let ladybugData = node["ladybug"];
-            $('#code-wrapper').remove();
-            $('#details-row').after('<pre id="code-wrapper" class="prettify"><code id="code" class="lang-xml"></code></pre>');
-            $('#code').text(ladybugData["message"]);
-            console.log("ladybugData");
-            console.log(ladybugData["message"]);
-            $scope.reportDetails = {
-                data: ladybugData,
-                text: ladybugData["message"],
-                values: {
-                    "Name:": ladybugData["name"],
-                    "Thread name:": ladybugData["threadName"],
-                    "Source class name:": ladybugData["sourceClassName"],
-                    "Path:": "???",
-                    "Checkpoint UID:": ladybugData["uid"],
-                    "Number of characters:": "???",
-                    "EstimatedMemoryUsage:": ladybugData["estimatedMemoryUsage"]
-                },
-                nodeId: node.nodeId
-            };
-            let stubStrategySelect = ladybugData["stubStrategy"];
-            if (stubStrategySelect === undefined) {
-                // If node is checkpoint
-                stubStrategySelect = ladybugData["stub"];
-                $scope.stubStrategies = ["Follow report strategy", "No", "Yes"];
-                $scope.stubStrategySelect = $scope.stubStrategies[stubStrategySelect + 1];
-            } else {
-                // If node is report
-                $scope.stubStrategies = $scope.testtoolStubStrategies;
-                $scope.stubStrategySelect = stubStrategySelect;
-            }
-            $scope.$apply();
-            PR.prettyPrint()
-        };
-        $scope.logg = function () {
-            console.log($scope.filters);
-        }
-
-        $scope.toggleEdit = function () {
-            console.log("toggling");
-            let codeWrappers = $('#code-wrapper');
-            let htmlText = '<pre id="code-wrapper" class="prettify"><code id="code" class="lang-xml"></code></pre>';
-            let buttonText = 'Edit';
-
-            if (codeWrappers.get().length === 0 || codeWrappers.get()[0].tagName === "PRE") {
-                let rows = Math.min(20, $scope.reportDetails.text.split('\n').length);
-                htmlText = '<div id="code-wrapper" class=\"form-group\"><textarea class=\"form-control\" id=\"code\" rows="' + rows + '"></textarea></div>';
-                buttonText = 'Save';
-            }
-            // TODO: Save the text|
-            codeWrappers.remove();
-            $('#details-edit').text(buttonText);
-            $('#details-row').after(htmlText);
-            $('#code').text($scope.reportDetails.text);
-        }
         // TODO: Move
         // $scope.deleteSelected = function () {
         //     let tree = $('#tree').treeview(true);
@@ -142,46 +75,6 @@ angular.module('myApp.view1', ['ngRoute'])
         //             alert(response.data);
         //         });
         // }
-
-        $scope.collapse = function () {
-            $('#tree').treeview('collapseNode', $scope.reportDetails.nodeId);
-        }
-        $scope.expand = function () {
-            let tree = $('#tree').treeview(true);
-            let node = tree.getNode($scope.reportDetails.nodeId);
-            let path = [];
-            path.push(node.nodeId);
-            while (node.parentId !== undefined) {
-                node = tree.getNode(node.parentId);
-                path.push(node.nodeId);
-            }
-            console.log("Expanding");
-            console.log(path);
-            for (let i = path.length - 1; i >= 0; i--) {
-                tree.expandNode(path[i]);
-            }
-        }
-
-        $scope.rerun = function () {
-            let rootNode = $scope.getRootNode();
-            let data = {};
-            data[$scope.storage] = [rootNode["ladybug"]["storageId"]];
-            console.log("RERUN");
-            console.log(data);
-            $http.post($scope.apiUrl + "/runner/run/" + $scope.storage, data)
-                .then(function (response) {
-                    console.log(response);
-                }, function (response) {
-                    console.log(response);
-                });
-        };
-
-
-        $scope.copyReport = function (to) {
-            let data = {};
-            data[$scope.storage] = [$scope.getRootNode()["ladybug"]["storageId"]];
-            $http.put($scope.apiUrl + "/report/store/" + to, data);
-        }
 
         $scope.updateOptions();
     }]);
