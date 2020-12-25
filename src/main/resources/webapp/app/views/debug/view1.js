5c0d89334ae9777031ba559f1b1dc00a45eb8ef9
Murat Kaan Meral <muratkaanmeral@gmail.com>
2020-12-25 14:34:25 +0100

    Add tree component

--- src/main/resources/webapp/app/views/debug/view1.js
+++ src/main/resources/webapp/app/views/debug/view1.js
@@ -13,9 +13,8 @@ angular.module('myApp.view1', ['ngRoute'])
         $scope.apiUrl = "http://localhost:8080/ibis_adapterframework_test_war_exploded/ladybug";
         $scope.columns = ['storageId', 'endTime', 'duration', 'name', 'correlationId', 'status', 'nrChpts', 'estMemUsage', 'storageSize'];
         $scope.storage = "debugStorage";
-        $scope.treeData = [];
-        $scope.reports = [];
         $scope.reportDetails = {text: "", values: {}};
+        $scope.table2tree = {};
         $scope.limit = 5;
         $scope.filters = {"limit": $scope.limit};
         $scope.options = {
@@ -63,7 +62,12 @@ angular.module('myApp.view1', ['ngRoute'])
         /*
          * Displays a selected reports in the content pane.
          */
-        $scope.display_report = function (event, node) {
+        $scope.display_report = function (rootNode, event, node) {
+            $scope.selectedNode = rootNode;
+            if (node === null) {
+                $scope.reportDetails = {text: "", values: {}};
+                return;
+            }
             let ladybugData = node["ladybug"];
             $('#code-wrapper').remove();
             $('#details-row').after('<pre id="code-wrapper" class="prettify"><code id="code" class="lang-xml"></code></pre>');
@@ -98,103 +102,6 @@ angular.module('myApp.view1', ['ngRoute'])
             $scope.$apply();
             PR.prettyPrint()
         };
-
-        $scope.updateTree = function () {
-            $('#tree').treeview({
-                data: $scope.treeData,
-                onNodeSelected: $scope.display_report,
-                onNodeUnselected: function (event, node) {
-                    console.log("UNSELECTED");
-                    console.log(event);
-                    console.log(node);
-                    $scope.reportDetails = {text: "", values: {}};
-                }
-            });
-        }
-
-        $scope.remove_circulars = function (node) {
-            if ("parent" in node) {
-                delete node["parent"];
-            }
-            if ("nodes" in node) {
-                for (let i = 0; i < node["nodes"].length; i++) {
-                    $scope.remove_circulars(node["nodes"][i]);
-                }
-            }
-        };
-
-        /*
-         * Adds the given report data to the tree.
-         */
-        $scope.addTree = function (data){
-            console.log("CALLED ADDTREE!!!!");
-            $scope.reports[data["storageId"]] = data;
-            let report = {
-                text: data["name"],
-                ladybug: data,
-                icon: "fa fa-file-code",
-                nodes: []
-            };
-            let checkpoints = data["checkpoints"];
-            let nodes = report.nodes;
-            let previous_node = null;
-            for (let i = 0; i < checkpoints.length; i++) {
-                let chkpt = checkpoints[i];
-                let node = {text: chkpt["name"], ladybug: chkpt, level: chkpt["level"]};
-
-                if (previous_node === null) {
-                    nodes.push(node);
-                    previous_node = node;
-                    console.log(" -- Added: " + node.text);
-                    continue;
-                }
-                let diff = (node.level - 1) - previous_node.level;
-
-                while (diff !== 0) {
-                    console.log("aim: " + node.level);
-                    console.log("prev: " + previous_node.level);
-                    console.log("Prev Text: " + previous_node.text);
-                    console.log("diff: " + diff);
-                    if (diff < 0) {
-                        previous_node = previous_node.parent;
-                    } else {
-                        if (!("nodes" in previous_node) || previous_node["nodes"] === null) {
-                            break;
-                        }
-                        previous_node = previous_node.nodes[previous_node.nodes - 1];
-                    }
-                    diff = (node.level - 1) - previous_node.level;
-                }
-                if (!("nodes" in previous_node) || previous_node["nodes"] === null) {
-                    previous_node["nodes"] = [];
-                }
-                previous_node["nodes"].push(node);
-                node.parent = previous_node;
-                previous_node = node;
-                console.log(" -- Added: " + node.ladybug["index"] + " " + node.text);
-                console.log(report);
-            }
-            $scope.remove_circulars(report);
-            $scope.treeData.push(report);
-            console.log($scope.treeData);
-            // Update tree
-            $scope.updateTree();
-        };
-
-        $scope.closeAll = function () {
-            $scope.treeData = [];
-            $('#tree').treeview({data: []});
-            $scope.$apply();
-        };
-
-        $scope.expandAll = function () {
-            $('#tree').treeview('expandAll', {levels: 99, silent: true});
-        }
-
-        $scope.collapseAll = function () {
-            $('#tree').treeview('collapseAll', {silent: true});
-        }
-
         $scope.logg = function () {
             console.log($scope.filters);
         }
@@ -215,17 +122,6 @@ angular.module('myApp.view1', ['ngRoute'])
             $('#details-edit').text(buttonText);
             $('#details-row').after(htmlText);
             $('#code').text($scope.reportDetails.text);
-        }
-
-        $scope.getRootNode = function () {
-            let tree = $('#tree').treeview(true);
-            if (tree.getSelected().length === 0)
-                return;
-            let node = tree.getSelected()[0];
-            while (node.parentId !== undefined) {
-                node = tree.getNode(node.parentId);
-            }
-            return node;
         }
         // TODO: Move
         // $scope.deleteSelected = function () {
@@ -285,18 +181,6 @@ angular.module('myApp.view1', ['ngRoute'])
             let data = {};
             data[$scope.storage] = [$scope.getRootNode()["ladybug"]["storageId"]];
             $http.put($scope.apiUrl + "/report/store/" + to, data);
-        }
-
-        $scope.close = function () {
-            let storageId = $scope.getRootNode()["ladybug"]["storageId"];
-            for (let i = 0; i < $scope.treeData.length; i++) {
-                if (storageId === $scope.treeData[i]["ladybug"]["storageId"]) {
-                    $scope.treeData.splice(i, 1);
-                    i--;
-                }
-            }
-            $scope.updateTree();
-            $scope.reportDetails = {text: "", values: {}};
         }
 
         $scope.updateOptions();
