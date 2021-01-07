c5e3e6086d4008eb0c472cf03d8c184dca3df4df
Murat Kaan Meral <muratkaanmeral@gmail.com>
2021-01-07 11:51:40 +0100

    Implement first version of compare page

--- src/main/resources/webapp/app/views/compare/view3.js
+++ src/main/resources/webapp/app/views/compare/view3.js
@@ -4,11 +4,25 @@ angular.module('myApp.view3', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view3', {
-    templateUrl: 'view3/view3.html',
+    templateUrl: 'views/compare/view3.html',
     controller: 'View3Ctrl'
   });
 }])
 
-.controller('View3Ctrl', [function() {
+.controller('View3Ctrl', ['$scope', function($scope) {
+  $scope.left_table2tree = {};
+  $scope.left_tree2display = {};
+  $scope.right_table2tree = {};
+  $scope.right_tree2display = {};
+  $scope.left_show = true;
+  $scope.right_show = true;
 
+  $scope.left_tree2display.select = function (rootNode, event, node) {
+      $scope.left_show = node === null;
+    $scope.$apply();
+  };
+  $scope.right_tree2display.select = function (rootNode, event, node) {
+    $scope.right_show = node === null;
+    $scope.$apply();
+  };
 }]);
