f0d92a95ed9bf5a258aba0b48e4599b44c53e8cb
Murat Kaan Meral <muratkaanmeral@gmail.com>
2021-02-04 10:14:50 +0100

    Implement SecurityContext and debug application context

--- src/main/java/nl/nn/testtool/web/api/ApiBase.java
+++ src/main/java/nl/nn/testtool/web/api/ApiBase.java
@@ -1,26 +1,27 @@
 package nl.nn.testtool.web.api;
 
+import nl.nn.testtool.SecurityContext;
 import nl.nn.testtool.storage.Storage;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Context;
+import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
-public abstract class ApiBase implements ApplicationContextAware {
+public abstract class ApiBase implements SecurityContext {
 	@Context
-	private HttpServletRequest httpRequest;
+	protected HttpServletRequest httpRequest;
 
-	ApplicationContext appContext;
+	protected static ApplicationContext applicationContext;
 	protected static Map<String, Storage> storages;
 
 	protected Object getBeanObject(String beanName) {
-		if (appContext == null)
-			throw new ApiException("APPLICATION CONTEXT IS NULL!!!!");
+		if (applicationContext == null)
+			throw new ApiException("Api could not access the ladybug context.");
 
-		Object bean = appContext.getBean(beanName);
+		Object bean = applicationContext.getBean(beanName);
 		if (bean == null)
 			throw new ApiException("Could not retrieve the requested bean [" + beanName + "]");
 		return bean;
@@ -50,8 +51,27 @@ public abstract class ApiBase implements ApplicationContextAware {
 	}
 
 	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		System.err.println("SETTING THE APPLICATION CONTEXT!!!");
-		appContext = applicationContext;
+	public Principal getUserPrincipal() {
+		return httpRequest.getUserPrincipal();
+	}
+
+	@Override
+	public boolean isUserInRoles(List<String> roles) {
+		if (getUserPrincipal() == null) {
+			// The servlet container didn't authenticate the user (not
+			// configured in the web.xml or explicitly overwritten by the
+			// servlet container (e.g. when running locally in WSAD)). In this
+			// case allow everything.
+			return true;
+		}
+		for (String role : roles) {
+			if (httpRequest.isUserInRole(role))
+				return true;
+		}
+		return false;
+	}
+
+	public static void setApplicationContext(ApplicationContext context) {
+		applicationContext = context;
 	}
 }
