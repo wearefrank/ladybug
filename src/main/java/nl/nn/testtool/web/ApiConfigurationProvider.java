2f6917824a914d2645c7afa1cf521adf15964b0b
Murat Kaan Meral <muratkaanmeral@gmail.com>
2021-02-04 09:45:41 +0100

    Create a class to provide configuration and application context to api
    (cherry picked from commit 361ba2b0d59d85dd0450ea545b2c37c9d6c4cc20)

---
+++ src/main/java/nl/nn/testtool/web/ApiConfigurationProvider.java
@@ -0,0 +1,22 @@
+package nl.nn.testtool.web;
+
+import nl.nn.testtool.web.api.ApiBase;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.List;
+import java.util.Map;
+
+public class ApiConfigurationProvider implements ApplicationContextAware {
+	Map<String, List<String>> authenticationRolesMap;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		ApiBase.setApplicationContext(applicationContext);
+	}
+
+	public void setAuthenticationRolesMap(Map<String, List<String>> authenticationRolesMap) {
+		AuthenticationInterceptor.setRolesMap(authenticationRolesMap);
+	}
+}
