4bb5d0c8c5b2a784aedd46b6f7a6158d5b52ebed
Murat Kaan Meral <muratkaanmeral@gmail.com>
2021-02-04 10:23:07 +0100

    Change import for authenticationInterceptor due to refactor
    (cherry picked from commit 770470c52c7b123ecb519a0d97e9a1f78a7ed636)

--- src/main/java/nl/nn/testtool/web/ApiConfigurationProvider.java
+++ src/main/java/nl/nn/testtool/web/ApiConfigurationProvider.java
@@ -1,6 +1,7 @@
 package nl.nn.testtool.web;
 
 import nl.nn.testtool.web.api.ApiBase;
+import nl.nn.testtool.web.interceptors.AuthenticationInterceptor;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
