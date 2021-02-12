373d6486c372e46e37750c1beaebd943e714873b
Murat Kaan Meral <muratkaanmeral@gmail.com>
2021-02-12 14:13:58 +0100

    Debug import, digestion error, and getmetadata call

--- src/main/resources/webapp/app/views/test/view2.js
+++ src/main/resources/webapp/app/views/test/view2.js
@@ -4,7 +4,7 @@ angular.module('myApp.view2', ['ngRoute'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view2', {
-            templateUrl: 'view2/view2.html',
+            templateUrl: 'views/test/view2.html',
             controller: 'View2Ctrl'
         });
     }])
@@ -87,11 +87,11 @@ angular.module('myApp.view2', ['ngRoute'])
             for (let i = 0; i < nodes.length; i++) {
                 $scope.openNode(nodes[i], true);
             }
-            $scope.$apply();
+            if (!$scope.$$phase) $scope.$apply();
         };
 
         $scope.updateTree = function () {
-            $http.get($scope.apiUrl + "/metadata/" + $scope.storage)
+            $http.get($scope.apiUrl + "/metadata/" + $scope.storage + "?path=&storageId=&status=")
                 .then(function (response) {
                     let fields = response.data["fields"];
                     let values = response.data["values"];
