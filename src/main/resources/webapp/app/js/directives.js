afcff457081e724435b529dbb65e08aede527de8
Murat Kaan Meral <muratkaanmeral@gmail.com>
2021-02-25 15:37:45 +0100

    Add directive for file upload

---
+++ src/main/resources/webapp/app/js/directives.js
@@ -0,0 +1,11 @@
+angular.module('myApp')
+
+.directive('customOnChange', function() {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var onChangeHandler = scope.$eval(attrs.customOnChange);
+            element.bind('change', onChangeHandler);
+        }
+    };
+});
