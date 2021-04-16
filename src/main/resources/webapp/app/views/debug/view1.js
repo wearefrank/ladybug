955a5a392284b4e4ebc00049c38514aa21313595
Murat Kaan <muratkaanmeral@gmail.com>
2021-04-16 12:42:30 +0200

    Cleanup code

--- src/main/resources/webapp/app/views/debug/view1.js
+++ src/main/resources/webapp/app/views/debug/view1.js
@@ -9,28 +9,9 @@ angular.module('myApp.view1', ['ngRoute'])
         });
     }])
 
-    .controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
+    .controller('View1Ctrl', ['$scope', function ($scope) {
         $scope.apiUrl = "http://localhost:8080/ibis_adapterframework_test_war_exploded/ladybug";
         $scope.storage = "debugStorage";
         $scope.table2tree = {};
         $scope.tree2display = {};
-        // TODO: Move
-        // $scope.deleteSelected = function () {
-        //     let tree = $('#tree').treeview(true);
-        //     if (tree.getSelected().length === 0)
-        //         return;
-        //     let node = tree.getSelected()[0];
-        //     while (node.parentId !== undefined) {
-        //         node = tree.getNode(node.parentId);
-        //     }
-        //     console.log("DELETE");
-        //     console.log(node["ladybug"]["storageId"]);
-        //     $http.delete($scope.apiUrl + "/report/" + $scope.storage + "/" + node["ladybug"]["storageId"])
-        //         .then(function () {
-        //             tree.remove(node);
-        //         }, function (response) {
-        //             console.log(response);
-        //             alert(response.data);
-        //         });
-        // }
     }]);
