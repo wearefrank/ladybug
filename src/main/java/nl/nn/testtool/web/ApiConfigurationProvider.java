bcdefb6e6bb8d8b5718bafd0c7040b2dcb52c22a
Murat Kaan <muratkaanmeral@gmail.com>
2021-05-20 13:57:23 +0200

    Add Javadoc for AuthenticationInterceptor

--- src/main/java/nl/nn/testtool/web/ApiConfigurationProvider.java
+++ src/main/java/nl/nn/testtool/web/ApiConfigurationProvider.java
@@ -27,11 +27,26 @@ import java.util.Map;
 public class ApiConfigurationProvider implements ApplicationContextAware {
 	Map<String, List<String>> authenticationRolesMap;
 
+	/**
+	 * This method is crucial to connect the ApplicationContext from framework, with the context from Servlets of the
+	 * api.
+	 * @param applicationContext Application context from the framework.
+	 * @throws BeansException
+	 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		ApiBase.setApplicationContext(applicationContext);
 	}
 
+	/**
+	 * Sets the roles map for {@link AuthenticationInterceptor}. {@link AuthenticationInterceptor} is used to filter
+	 * the requests based on their permission requirements.
+	 *
+	 * @param authenticationRolesMap is a map explaining {@link AuthenticationInterceptor} configuration, where keys
+	 *                                  are strings explaining possible requests (such as "POST,PUT/path/to/endpoint").
+	 *                                  Method part can be left empty in order to apply it for all methods
+	 *                                  (such as "/path/to/endpoint").
+	 */
 	public void setAuthenticationRolesMap(Map<String, List<String>> authenticationRolesMap) {
 		AuthenticationInterceptor.setRolesMap(authenticationRolesMap);
 	}
