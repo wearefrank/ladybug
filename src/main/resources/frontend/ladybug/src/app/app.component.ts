38b5072ce28e5d4bd5b9b80bfebc38de4b930eba
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2021-08-11 15:07:36 +0200

    Make tabs dynamic

--- src/main/resources/frontend/ladybug/src/app/app.component.ts
+++ src/main/resources/frontend/ladybug/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-// import { ngbModule } from '@ng-bootstrap/ng-bootstrap';
+import {Component} from '@angular/core';
+import {DebugComponent} from "./debug/debug.component";
+import {TestComponent} from "./test/test.component";
+import {CompareComponent} from "./compare/compare.component";
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ladybug';
+  active = 1;
+  tabs: {key: string, value: any}[] = [
+    {key: 'Debug', value: DebugComponent},
+    {key: 'Test', value: TestComponent},
+    {key: 'Compare', value: CompareComponent}
+  ]
+
 }
