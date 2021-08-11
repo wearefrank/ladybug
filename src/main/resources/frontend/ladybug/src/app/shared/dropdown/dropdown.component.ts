da83d3bbbed1a7a7cdc6d41554c086658e5da344
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2021-08-11 15:39:31 +0200

    Add dropdowns

--- src/main/resources/frontend/ladybug/src/app/shared/dropdown/dropdown.component.ts
+++ src/main/resources/frontend/ladybug/src/app/shared/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent implements OnInit {
+  @Input()
+  get items(): string[]{return this._items}
+  set items(items: string[]) { this._items = items; }
+  private _items: string[] = []
+
+  @Input()
+  get title(): string { return this._title }
+  set title(title: string) { this._title = title; }
+  private _title = "";
+
+  @Input()
+  get icon(): string {return this._icon}
+  set icon(icon: string){ this._icon = icon; }
+  private _icon = "";
 
   constructor() { }
 
