9987bf5d001f3aefb4bef1fa6fb9b2cdfa7b45a2
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2021-08-17 10:09:06 +0200

    Add dynamic showing of components

--- src/main/resources/frontend/ladybug/src/app/shared/display/display.component.ts
+++ src/main/resources/frontend/ladybug/src/app/shared/display/display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-display',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
+  @Input() editing: boolean = false
+  @Input() displayReport: boolean = false
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
