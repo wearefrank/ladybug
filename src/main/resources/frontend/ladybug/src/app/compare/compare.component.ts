d2132dc12574d84cb8813c509e66941ebdc6a232
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2021-08-30 14:17:28 +0200

    Fix table selection in compare

--- src/main/resources/frontend/ladybug/src/app/compare/compare.component.ts
+++ src/main/resources/frontend/ladybug/src/app/compare/compare.component.ts
@@ -22,6 +22,33 @@ export class CompareComponent {
   rightReportSelected: boolean = false;
   constructor() { }
 
+  addReportLeft(newReport: any) {
+    if (this.leftId === newReport.id) {
+      this.leftReports.push(newReport);
+      this.leftTreeComponent?.handleChange(this.leftReports);
+    }
+  }
+
+  addReportRight(newReport: any) {
+    if (this.rightId === newReport.id) {
+      this.rightReports.push(newReport)
+      this.rightTreeComponent?.handleChange(this.rightReports);
+    }
+  }
+
+  selectReportLeft(currentReport: any) {
+    this.leftReportSelected = true;
+    this.leftCurrentReport = currentReport;
+    console.log(this.leftDisplayComponent)
+    this.leftDisplayComponent?.showReport(this.leftCurrentReport);
+  }
+
+  selectReportRight(currentReport: any) {
+    this.rightReportSelected = true;
+    this.rightCurrentReport = currentReport;
+    this.rightDisplayComponent?.showReport(this.rightCurrentReport);
+  }
+
   /**
    * Add a new report to the specific tree (left or right)
    * @param newReport - the report to be added to the specific tree
@@ -41,12 +68,15 @@ export class CompareComponent {
    * @param currentReport - the report to be viewed
    */
   selectReport(currentReport: any) {
+    console.log(currentReport.ladybug.id)
     if (currentReport.ladybug.id === this.leftId) {
       this.leftReportSelected = true;
+      console.log("Left: " + this.leftReportSelected + ", right: " + this.rightReportSelected)
       this.leftCurrentReport = currentReport;
       this.leftDisplayComponent?.showReport(currentReport);
-    } else {
+    } else if (currentReport.ladybug.id === this.rightId){
       this.rightReportSelected = true;
+      console.log("Left: " + this.leftReportSelected + ", right: " + this.rightReportSelected)
       this.rightCurrentReport = currentReport;
       this.rightDisplayComponent?.showReport(currentReport);
     }
