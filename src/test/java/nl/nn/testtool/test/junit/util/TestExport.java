59f9539f2f37d94b9baefb7e80dcdbfc49005563
Martijn Dirkse <martijn@integrationpartners.nl>
2022-01-06 18:13:09 +0100

    Fix failure on storage id and enable all memory storage tests

--- src/test/java/nl/nn/testtool/test/junit/util/TestExport.java
+++ src/test/java/nl/nn/testtool/test/junit/util/TestExport.java
@@ -134,7 +134,7 @@ public class TestExport {
 		List<Checkpoint> checkpoints = new ArrayList<Checkpoint>();
 		checkpoints.add(checkpoint);
 		report.setCheckpoints(checkpoints);
-		assertExport(RESOURCE_PATH, "test", report, false, false, false);
+		assertExport(RESOURCE_PATH, "test", report, false, false, false, true);
 	}
 
 	private static void getBeanProperties(Class<?> clazz, String verb, Map<String, Method> beanProperties) {
@@ -157,8 +157,12 @@ public class TestExport {
 		}
 	}
 
+	// TODO: We have too many boolean arguments, not clear. We can make a new class, say XmlComparator,
+	// that has setters for all the ignores that we need. Then objects of that class can perform the
+	// comparing.
 	public static void assertExport(String resourcePath, String testCaseName, Report report,
-			boolean applyToXmlIgnores, boolean applyEpochTimestampIgnores, boolean applyStackTraceIgnores)
+			boolean applyToXmlIgnores, boolean applyEpochTimestampIgnores, boolean applyStackTraceIgnores,
+			boolean ignoreStorageId)
 			throws IOException, StorageException {
 		byte[] bytes = Export.getReportBytes(report);
 		ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
@@ -171,6 +175,9 @@ public class TestExport {
 		}
 		String actual = stringBuffer.toString();
 		actual = ReportRelatedTestCase.applyXmlEncoderIgnores(actual);
+		if (ignoreStorageId) {
+			actual = ReportRelatedTestCase.ignoreStorageId(actual, report);
+		}
 		if (applyToXmlIgnores) {
 			actual = ReportRelatedTestCase.applyToXmlIgnores(actual, report);
 		}
