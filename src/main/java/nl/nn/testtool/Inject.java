9246390b4e31a383831578e8a49c8fee3c9a2229
Jaco de Groot <jaco.de.groot@ibissource.org>
2023-01-20 23:54:30 +0100

    Document dummy Inject

--- src/main/java/nl/nn/testtool/Inject.java
+++ src/main/java/nl/nn/testtool/Inject.java
@@ -15,6 +15,32 @@
 */
 package nl.nn.testtool;
 
+/**
+ * The pom.xml will replace javax.inject.Inject to this dummy to prevent problems with WebSphere when Enable-CDI is not
+ * set to false (see also: https://github.com/ibissource/iaf/pull/4211/files):
+ * <pre>
+ * com.ibm.ws.exception.RuntimeWarning: com.ibm.ws.exception.RuntimeError: com.ibm.ws.exception.RuntimeError: java.lang.RuntimeException: com.ibm.ws.cdi.CDIRuntimeException: com.ibm.ws.cdi.CDIDeploymentRuntimeException: org.jboss.weld.exceptions.DeploymentException: Exception List with 6 exceptions:
+ * Exception 0 :
+ * org.jboss.weld.exceptions.DeploymentException: WELD-001408: Unsatisfied dependencies for type List&lt;String&gt; with qualifiers @Default
+ *   at injection point [BackedAnnotatedField] @Inject @Autowired private nl.nn.testtool.filter.View.metadataNames
+ *   at nl.nn.testtool.filter.View.metadataNames(View.java:0)
+ *   at org.jboss.weld.bootstrap.Validator.validateInjectionPointForDeploymentProblems(Validator.java:362)
+ *   at org.jboss.weld.bootstrap.Validator.validateInjectionPoint(Validator.java:284)
+ *   at org.jboss.weld.bootstrap.Validator.validateGeneralBean(Validator.java:137)
+ *   at org.jboss.weld.bootstrap.Validator.validateRIBean(Validator.java:158)
+ *   at org.jboss.weld.bootstrap.Validator.validateBean(Validator.java:501)
+ *   at org.jboss.weld.bootstrap.ConcurrentValidator$1.doWork(ConcurrentValidator.java:61)
+ *   at org.jboss.weld.bootstrap.ConcurrentValidator$1.doWork(ConcurrentValidator.java:59)
+ *   at org.jboss.weld.executor.IterativeWorkerTaskFactory$1.call(IterativeWorkerTaskFactory.java:62)
+ *   at org.jboss.weld.executor.IterativeWorkerTaskFactory$1.call(IterativeWorkerTaskFactory.java:55)
+ *   at java.util.concurrent.FutureTask.run(FutureTask.java:277)
+ *   at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1160)
+ *   at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:635)
+ *   at java.lang.Thread.run(Thread.java:825)
+ * </pre>
+ * 
+ * @author Jaco de Groot
+ */
 public @interface Inject {
 
 }
