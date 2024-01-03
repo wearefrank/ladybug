4afda1f5a8c8b5c9a8b5770645922d2e158c1078
Jaco de Groot <jaco.de.groot@ibissource.org>
2024-01-03 17:22:14 +0100

    Fix compile error

--- src/main/java/nl/nn/testtool/storage/proofofmigration/ProofOfMigrationStorage.java
+++ src/main/java/nl/nn/testtool/storage/proofofmigration/ProofOfMigrationStorage.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2022-2023 WeAreFrank!
+   Copyright 2022-2024 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -73,15 +73,6 @@ public class ProofOfMigrationStorage extends DatabaseStorage {
 			return "MESSAGES";
 		} else {
 			return table;
-		}
-	}
-
-	@Override
-	public boolean isCheckTableExists() {
-		if (checkTableExists == null) {
-			return false;
-		} else {
-			return checkTableExists;
 		}
 	}
 
