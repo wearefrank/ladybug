e162126af4acd514f49c18fd23aee1097d3012a1
ricklue <s1135986@student.hsleiden.nl>
2024-06-18 16:28:40 +0200

    Feat: instrumented addCheckpoint to send data to zipkin

--- src/main/java/nl/nn/testtool/Report.java
+++ src/main/java/nl/nn/testtool/Report.java
@@ -30,6 +30,11 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -47,6 +52,7 @@ import nl.nn.testtool.transform.ReportXmlTransformer;
 import nl.nn.testtool.util.CsvUtil;
 import nl.nn.testtool.util.EscapeUtil;
 import nl.nn.testtool.util.XmlUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author Jaco de Groot
@@ -127,6 +133,14 @@ public class Report implements Serializable {
 	private transient boolean logMaxMemoryUsage = true;
 	private transient Map<Object, Set<Checkpoint>> streamingMessageListeners = new HashMap<>();
 	private transient Map<Object, StreamingMessageResult> streamingMessageResults = new HashMap<>();
+	private final OpenTelemetry openTelemetry;
+	private final Tracer tracer;
+
+	@Autowired
+	Report (OpenTelemetry openTelemetry) {
+		this.openTelemetry = openTelemetry;
+		this.tracer = openTelemetry.getTracer(Report.class.getName(), "0.1.0");
+	}
 
 	@Transient
 	@JsonIgnore
@@ -504,6 +518,17 @@ public class Report implements Serializable {
 			StubableCode stubableCode, StubableCodeThrowsException stubableCodeThrowsException,
 			Set<String> matchingStubStrategies, int checkpointType, Integer index, Integer level) {
 		Checkpoint checkpoint = new Checkpoint(this, threadName, sourceClassName, name, checkpointType, level);
+		SpanBuilder checkpointSpanBuilder = tracer.spanBuilder("checkpoint - " + name);
+		for (Checkpoint checkpointInList: checkpoints) {
+			if (checkpointInList.getType() == 1 && checkpointInList.getLevel() == checkpoint.getLevel() - 1) {
+				checkpointSpanBuilder.setParent(Context.current().with(checkpointInList.getSpan()));
+			}
+		}
+		Span checkpointSpan = checkpointSpanBuilder.startSpan();
+		checkpointSpan.setAttribute("checkpointType", checkpointType);
+		checkpointSpan.setAttribute("checkpointTypeAsString", checkpoint.getTypeAsString());
+		checkpointSpan.setAttribute("checkpointLevel", checkpoint.getLevel());
+		checkpoint.setSpan(checkpointSpan);
 		boolean stub = false;
 		if (originalReport != null) {
 			Path path = checkpoint.getPath(true);
@@ -583,6 +608,17 @@ public class Report implements Serializable {
 		if (log.isDebugEnabled()) {
 			log.debug("Added checkpoint " + getCheckpointLogDescription(name, checkpointType, level));
 		}
+		if (checkpointType != 1) {
+			checkpoint.getSpan().end();
+		}
+		if (checkpointType == 2) {
+			for (Checkpoint checkpointInList: checkpoints) {
+				if (checkpointInList.getType() == 1 && checkpointInList.getLevel() == checkpoint.getLevel() - 1) {
+					checkpointInList.getSpan().end();
+				}
+			}
+		}
+
 		return message;
 	}
 
@@ -859,7 +895,7 @@ public class Report implements Serializable {
 
 	@Override
 	public Report clone() throws CloneNotSupportedException {
-		Report report = new Report();
+		Report report = new Report(openTelemetry);
 		report.setTestTool(testTool);
 		report.setStartTime(startTime);
 		report.setEndTime(endTime);
