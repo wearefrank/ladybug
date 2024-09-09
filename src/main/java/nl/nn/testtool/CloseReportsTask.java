5d16619d6663b2cf18ca2a01747372ebd4b6e719
Jaco de Groot <jaco.de.groot@ibissource.org>
2024-09-09 19:08:27 +0200

    Prevent NPE in CloseReportsTask

--- src/main/java/nl/nn/testtool/CloseReportsTask.java
+++ src/main/java/nl/nn/testtool/CloseReportsTask.java
@@ -15,6 +15,11 @@
 */
 package nl.nn.testtool;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import lombok.Setter;
 
 /**
@@ -30,6 +35,7 @@ import lombok.Setter;
  * @author Jaco de Groot
  */
 public class CloseReportsTask {
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	private @Setter TestTool testTool;
 	private @Setter long threadsTime = 60 * 1000;
 	private @Setter long messageCapturersTime = 60 * 1000;
@@ -40,8 +46,19 @@ public class CloseReportsTask {
 	private @Setter long logThreadInfoMaxReportAge = 5 * 60 * 1000;
 
 	public void closeReports() {
-		testTool.close(threadsTime, messageCapturersTime, waitForMainThreadToFinish, logThreadInfoBeforeClose,
-				logThreadInfoMinReportAge, logThreadInfoMaxReportAge);
+		// Check whether initialized as it seems to be possible for this method to be called by the scheduler before
+		// initialization:
+		//    https://github.com/wearefrank/ladybug/issues/257
+		//    https://github.com/wearefrank/ladybug/issues/293
+		// It doesn't reproduce in the test webapp when adding a delay in TestTool.init(). In that case it works as
+		// expected and this method isn't called before TestTool.init() is finished and setTestTool() on this class has
+		// been called.
+		if (testTool != null) {
+			testTool.close(threadsTime, messageCapturersTime, waitForMainThreadToFinish, logThreadInfoBeforeClose,
+					logThreadInfoMinReportAge, logThreadInfoMaxReportAge);
+		} else {
+			log.debug("Skip execution because setTestTool() hasn't been called (yet)");
+		}
 	}
 
 }
