d59305710a42d27292b96a4d55b3b89ac06982fa
alisihab <sihab.akcan@gmail.com>
2025-05-23 15:35:41 +0200

    normalize path returns null for null

--- src/main/java/nl/nn/testtool/web/api/ReportApi.java
+++ src/main/java/nl/nn/testtool/web/api/ReportApi.java
@@ -313,9 +313,7 @@ public class ReportApi extends ApiBase {
 			if (report == null)
 				return Response.status(Response.Status.NOT_FOUND).entity("Could not find report with storageId [" + storageId + "]").build();
 			report.setName(map.get("name"));
-			if (map.get("path") != null) {
-				report.setPath(TestComponent.normalizePath(map.get("path")));
-			}
+			report.setPath(TestComponent.normalizePath(map.get("path")));
 			report.setDescription(map.get("description"));
 			report.setTransformation(map.get("transformation"));
 			report.setStubStrategy(map.get("stubStrategy"));
