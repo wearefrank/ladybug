0ee28a7dce72e19d6defcc007c5cb4b5d7a78071
Martijn Dirkse <mhdirkse@pm.me>
2025-11-14 17:32:17 +0100

    Attempt to fix, does not work

--- src/main/java/nl/nn/testtool/web/api/LoginController.java
+++ src/main/java/nl/nn/testtool/web/api/LoginController.java
@@ -15,13 +15,29 @@
 */
 package nl.nn.testtool.web.api;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 class LoginController {
+	@ResponseBody
 	@GetMapping("/login")
-	String login() {
-		return "login";
+	public String login() {
+		return Arrays.asList(new String[] {
+			"<html>",
+			"<head><title>Login</title></head>",
+			"  <body>",
+			"    <form method=\"post\" action=\"/ladybug/api/login\">",
+			"      <input type=\"text\" name=\"username\" placeholder=\"Username\"/>",
+			"      <input type=\"password\" name=\"password\" placeholder=\"Password\"/>",
+			"      <button type=\"submit\">Login</button>",
+			"    </form>",
+			"  </body>",
+			"</html>"
+		}).stream().collect(Collectors.joining("\n"));
 	}
 }
