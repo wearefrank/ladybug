827285c7f3619932e54853eaa0ec73ff1cb35c1e
Martijn Dirkse <mhdirkse@pm.me>
2026-01-08 17:23:24 +0100

    Add log line when exception handler fires

--- ladybug-backend-springmvc/src/main/java/org/wearefrank/ladybug/web/springmvc/api/ExceptionHandlers.java
+++ ladybug-backend-springmvc/src/main/java/org/wearefrank/ladybug/web/springmvc/api/ExceptionHandlers.java
@@ -1,16 +1,22 @@
 package org.wearefrank.ladybug.web.springmvc.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.lang.invoke.MethodHandles;
+
 
 @RestControllerAdvice
 public class ExceptionHandlers {
+	private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	@ExceptionHandler
 	public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e) {
+		log.error("ExceptionHandlers.handleAccessDeniedException() captured exception", e);
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
 	}
 }
