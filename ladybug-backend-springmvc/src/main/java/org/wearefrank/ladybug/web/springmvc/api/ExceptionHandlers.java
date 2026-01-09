d58e03d49c71be11f77a1e908faf54ef7bd2664a
Martijn Dirkse <mhdirkse@pm.me>
2026-01-09 13:14:40 +0100

    Put the exact exception class in the handler class

--- ladybug-backend-springmvc/src/main/java/org/wearefrank/ladybug/web/springmvc/api/ExceptionHandlers.java
+++ ladybug-backend-springmvc/src/main/java/org/wearefrank/ladybug/web/springmvc/api/ExceptionHandlers.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,8 +20,8 @@ public class ExceptionHandlers {
 		log.error("Constructing org.wearefrank.ladybug.web.springmvc.api.ExceptionHandlers");
 	}
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handleAccessDeniedException(Exception e) {
+	@ExceptionHandler(AuthorizationDeniedException.class)
+	public ResponseEntity<?> handleAccessDeniedException(AuthorizationDeniedException e) {
 		log.error("ExceptionHandlers.handleAccessDeniedException() captured exception", e);
 		return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
 	}
