3abd789dc6d1775feecff034968d16fd06993415
Martijn Dirkse <mhdirkse@pm.me>
2026-01-19 17:32:23 +0100

    Fix previous commit

--- ladybug-common/src/main/java/org/wearefrank/ladybug/echo2/reports/ReportsComponent.java
+++ ladybug-common/src/main/java/org/wearefrank/ladybug/echo2/reports/ReportsComponent.java
@@ -757,8 +757,7 @@ public class ReportsComponent extends BaseComponent implements BeanParent, Actio
 	}
 
 	private Integer parseStorageId(Object o) {
-		if (o instanceof Integer integer) {
-				return integer;
+		if (o  instanceof Integer) {
 			return (Integer) o;
 		} else {
 			return Integer.valueOf((String) o);
