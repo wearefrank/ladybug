dffd50bdd4886f80557cd0318c4732e91e2f28f9
Martijn Dirkse <mhdirkse@pm.me>
2026-02-19 17:36:33 +0100

    Fix mvn build

--- ladybug-common/src/main/java/org/wearefrank/ladybug/TestTool.java
+++ ladybug-common/src/main/java/org/wearefrank/ladybug/TestTool.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2019-2025 WeAreFrank!, 2018 Nationale-Nederlanden
+   Copyright 2019-2026 WeAreFrank!, 2018 Nationale-Nederlanden
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
