c2dc785e975d70106379c14e447b31a3250dc786
Martijn Dirkse <mhdirkse@pm.me>
2026-02-24 18:16:20 +0100

    Test toXml with transformation that can make xml empty

--- ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/downloadReports/DownloadReportsTest.java
+++ ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/downloadReports/DownloadReportsTest.java
@@ -1,25 +1,50 @@
 package org.wearefrank.ladybug.test.junit.downloadReports;
 
+import io.micrometer.common.util.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 import org.springframework.context.ApplicationContext;
-import org.wearefrank.ladybug.MessageEncoderImpl;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.wearefrank.ladybug.Report;
 import org.wearefrank.ladybug.TestTool;
 import org.wearefrank.ladybug.storage.StorageException;
 import org.wearefrank.ladybug.test.junit.Common;
 import org.wearefrank.ladybug.test.junit.ReportRelatedTestCase;
+import org.wearefrank.ladybug.transform.ReportXmlTransformer;
+
+import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class DownloadReportsTest {
 	private ApplicationContext context = Common.CONTEXT_MEM_STORAGE;
+
+	private Resource reportTransformationForEmptyReports =
+			new ClassPathResource("/org/wearefrank/ladybug/test/junit/reportTransformationForEmptyXml.xslt");
+	private String transformerXslt;
 	private TestTool testTool = (TestTool)context.getBean("testTool");
 
 	private int storageIdOfFirst = 0;
 	private int storageIdOfSecond = 0;
 
+	public DownloadReportsTest() {
+		try {
+			transformerXslt = FileCopyUtils.copyToString(
+					new InputStreamReader(reportTransformationForEmptyReports.getInputStream(), StandardCharsets.UTF_8));
+		} catch(Exception e) {
+			System.out.println(String.format("Could not read resource for report transformation: %s", e.getStackTrace()));
+		}
+	}
+
 	@Before
-	public void setUp() throws StorageException {
+	public void setUp() throws StorageException, IOException {
 		String correlationIdOfFirst = ReportRelatedTestCase.getCorrelationId();
 		testTool.startpoint(correlationIdOfFirst, "FakeClassName", "DownloadReportsTest first report", "Message of DownloadReportsTest first report");
 		testTool.endpoint(correlationIdOfFirst, "FakeClassName", "DownloadReportsTest first report", "End of first");
@@ -30,15 +55,17 @@ public class DownloadReportsTest {
 		Report secondReport = ReportRelatedTestCase.findAndGetReport(testTool, testTool.getDebugStorage(), correlationIdOfSecond);
 		storageIdOfFirst = firstReport.getStorageId();
 		storageIdOfSecond = secondReport.getStorageId();
-		Assert.assertEquals(firstReport.getCheckpoints().size(), 2);
-		Assert.assertEquals(secondReport.getCheckpoints().size(), 2);
 	}
 
 	@Test
-	public void testReportsExist() throws StorageException {
+	public void testReportsExistAndAreAsNeeded() throws StorageException {
 		Report firstReport = testTool.getDebugStorage().getReport(storageIdOfFirst);
 		Report secondReport = testTool.getDebugStorage().getReport(storageIdOfSecond);
-		Assert.assertEquals(firstReport.getCheckpoints().size(), 2);
-		Assert.assertEquals(secondReport.getCheckpoints().size(), 2);
+		Assert.assertEquals(2, firstReport.getCheckpoints().size());
+		Assert.assertEquals(2, secondReport.getCheckpoints().size());
+		firstReport.setTransformation(transformerXslt);
+		secondReport.setTransformation(transformerXslt);
+		Assert.assertTrue(StringUtils.isNotEmpty(firstReport.toXml()));
+		Assert.assertEquals("", secondReport.toXml());
 	}
 }
