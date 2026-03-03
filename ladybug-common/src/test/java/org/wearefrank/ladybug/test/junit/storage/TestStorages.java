f957c3e6cef50b5485b124400662f7936da2ff92
Martijn Dirkse <mhdirkse@pm.me>
2026-03-03 11:40:07 +0100

    Add unit test, but it does not show the problem

--- ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/storage/TestStorages.java
+++ ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/storage/TestStorages.java
@@ -1,5 +1,8 @@
 package org.wearefrank.ladybug.test.junit.storage;
 
+import org.wearefrank.ladybug.Report;
+import org.wearefrank.ladybug.storage.CrudStorage;
+import org.wearefrank.ladybug.storage.LogStorage;
 import org.wearefrank.ladybug.storage.Storage;
 import org.wearefrank.ladybug.test.junit.ReportRelatedTestCase;
 import org.junit.Before;
@@ -8,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -36,6 +42,26 @@ public class TestStorages extends ReportRelatedTestCase {
         assertEquals(2, storage.getSize());
         storage.clear();
         assertEquals(0, storage.getSize());
+    }
+
+    @Test
+    public void testVariablesForCrudStorage() throws Exception {
+        System.out.println(String.format("TestStorages.testVariablesForCrudStorage(): storage is of type [%s]", storage.getClass().getName()));
+        if (! (storage instanceof CrudStorage)) {
+            System.out.println("Skip test testVariablesForCrudStorage() for other types of storage");
+            return;
+        }
+        createReport();
+        assertEquals(1, storage.getSize());
+        int storageId = storage.getStorageIds().get(0);
+        Report report = storage.getReport(storageId);
+        Map<String, String> variables = new HashMap<>();
+        variables.put("var1", "val1");
+        report.setVariables(variables);
+        ((CrudStorage) storage).update(report);
+        assertEquals(1, storage.getStorageIds().size());
+        Report recovered = storage.getReport(storageId);
+        assertEquals("val1", report.getVariables().get("var1"));
     }
 
     private void createReport() {
