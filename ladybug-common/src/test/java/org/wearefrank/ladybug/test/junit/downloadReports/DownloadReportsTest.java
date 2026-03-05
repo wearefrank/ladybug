41198b62bbe6c431cc8a268fdd14b61275924e4c
Martijn Dirkse <mhdirkse@pm.me>
2026-03-05 14:53:04 +0100

    Add unit tests that start at ReportApiImpl

--- ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/downloadReports/DownloadReportsTest.java
+++ ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/downloadReports/DownloadReportsTest.java
@@ -17,9 +17,20 @@ import org.wearefrank.ladybug.test.junit.ReportRelatedTestCase;
 import org.springframework.core.io.Resource;
 import org.wearefrank.ladybug.transform.ReportXmlTransformer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.wearefrank.ladybug.util.ExportResult;
+import org.wearefrank.ladybug.web.common.ReportApiImpl;
 
 public class DownloadReportsTest {
 	private ApplicationContext context = Common.CONTEXT_MEM_STORAGE;
@@ -32,6 +43,7 @@ public class DownloadReportsTest {
 	private String xsltForEmptyReports;
 	private String xsltThatLeavesTrace;
 	private TestTool testTool = (TestTool)context.getBean("testTool");
+	private ReportApiImpl reportApiImpl;
 
 	private int storageIdOfFirst = 0;
 	private int storageIdOfSecond = 0;
@@ -42,6 +54,9 @@ public class DownloadReportsTest {
 					new InputStreamReader(reportTransformationForEmptyReports.getInputStream(), StandardCharsets.UTF_8));
 			xsltThatLeavesTrace = FileCopyUtils.copyToString(
 					new InputStreamReader(reportTransformationThatLeavesTrace.getInputStream(), StandardCharsets.UTF_8));
+			// ReportApiImpl is not created as a bean when test Spring configuration is read.
+			reportApiImpl = new ReportApiImpl();
+			reportApiImpl.setTestTool(testTool);
 		} catch(Exception e) {
 			System.out.println(String.format("Could not read resource for report transformation: %s", e.getStackTrace()));
 		}
@@ -108,5 +123,57 @@ public class DownloadReportsTest {
 		secondReport.setGlobalReportXmlTransformer(global);
 		Assert.assertTrue(StringUtils.isNotEmpty(firstReport.toXml()));
 		Assert.assertEquals("", secondReport.toXml());
+	}
+
+	@Test
+	public void whenDownloadingOneReportSummaryRequestedThenXmlFile() throws Exception {
+		ExportResult result = reportApiImpl.downloadFile(
+				"testStorage", "false", "true", Arrays.asList(new Integer[] {storageIdOfFirst}));
+		Assert.assertTrue(result.getSuggestedFilename().endsWith(".xml"));
+	}
+
+	@Test
+	public void whenDownloadingOneReportRequestedThenTtsFile() throws Exception {
+		ExportResult result = reportApiImpl.downloadFile(
+				"testStorage", "true", "false", Arrays.asList(new Integer[] {storageIdOfFirst}));
+		Assert.assertTrue(result.getSuggestedFilename().endsWith(".ttr"));
+	}
+
+	@Test
+	public void whenDownloadingReportAndSummaryThenZipFile() throws Exception {
+		ExportResult result = reportApiImpl.downloadFile(
+				"testStorage", "true", "true", Arrays.asList(new Integer[] {storageIdOfFirst}));
+		Assert.assertTrue(result.getSuggestedFilename().endsWith(".zip"));
+		List<String> zipEntries = getZipEntries(result.getTempFile());
+		Assert.assertEquals(2, zipEntries.size());
+		boolean hasTtr = zipEntries.stream().anyMatch((s) -> s.endsWith("ttr"));
+		boolean hasXml = zipEntries.stream().anyMatch((s) -> s.endsWith("xml"));
+		Assert.assertTrue(hasTtr && hasXml);
+	}
+
+	@Test
+	public void whenDownloadingTwoSummariesThenZipWithTwoXml() throws Exception {
+		ExportResult result = reportApiImpl.downloadFile(
+				"testStorage", "false", "true", Arrays.asList(new Integer[] {storageIdOfFirst, storageIdOfSecond}));
+		Assert.assertTrue(result.getSuggestedFilename().endsWith(".zip"));
+		List<String> zipEntries = getZipEntries(result.getTempFile());
+		Assert.assertEquals(2, zipEntries.size());
+		Assert.assertTrue(zipEntries.stream().allMatch(s -> s.endsWith(".xml")));
+		boolean hasFirst = zipEntries.stream().anyMatch(s -> s.contains("first"));
+		boolean hasSecond = zipEntries.stream().anyMatch(s -> s.contains("second"));
+		Assert.assertTrue(hasFirst && hasSecond);
+	}
+
+	private List<String> getZipEntries(File f) throws IOException {
+		List<String> result = new ArrayList<>();
+		InputStream is = new FileInputStream(f);
+		ZipInputStream z = new ZipInputStream(is);
+		ZipEntry e;
+		while((e = z.getNextEntry()) != null) {
+			result.add(e.getName());
+			z.closeEntry();
+		}
+		z.close();
+		return result;
 	}
 }
