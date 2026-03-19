1a0e73cef782787ff895f42beb02115e44e9974b
Martijn Dirkse <mhdirkse@live.nl>
2026-03-19 14:16:17 +0100

    Simple UI updates (#670)
    * Save space to the top of the report component
    
    * Shorter button labels above checkpoint value
    
    * Same change for root node
    
    * Fix Cypress tests
    
    * Follow agreement from telephone call Jaco and Vivy
    
    * Fix cypress tests
    
    * Remove classes from report-buttons.html that have impact on styling

--- ladybug-frontend/cypress/e2e/no-profile/report/message-context.cy.ts
+++ ladybug-frontend/cypress/e2e/no-profile/report/message-context.cy.ts
@@ -12,9 +12,9 @@ describe('Message context', () => {
     cy.assertDebugTableLength(1).click();
     cy.checkFileTreeLength(1);
     cy.get('[data-cy-messagecontext-table="table"]').should('not.exist');
-    cy.get(':contains(Show messagecontext)').should('be.visible');
+    cy.get('[data-cy-open-messagecontext-table]').should('not.be.checked');
     cy.get('[data-cy-open-messagecontext-table]').click();
-    cy.get(':contains(Hide messagecontext)').should('be.visible');
+    cy.get('[data-cy-open-messagecontext-table]').should('be.checked');
     cy.get('[data-cy-messagecontext-table="table"]').should('exist');
     cy.get('[data-cy-messagecontext-table="value"]').should('not.exist');
   });
@@ -26,7 +26,7 @@ describe('Message context', () => {
     cy.assertDebugTableLength(1).click();
     cy.checkFileTreeLength(1);
     cy.clickRootNodeInFileTree();
-    cy.get(':contains(Show messagecontext)').should('not.exist');
+    cy.get('[data-cy-open-messagecontext-table]').should('not.exist');
     cy.get('[data-cy-messagecontext-table="table"]').should('not.exist');
   });
 
@@ -38,9 +38,9 @@ describe('Message context', () => {
     cy.checkFileTreeLength(1);
     cy.clickEndCheckpointOfThreeNodeReport();
     cy.get('[data-cy-messagecontext-table="table"]').should('not.exist');
-    cy.get(':contains(Show messagecontext)').should('be.visible');
+    cy.get('[data-cy-open-messagecontext-table]').should('not.be.checked');
     cy.get('[data-cy-open-messagecontext-table]').click();
-    cy.get(':contains(Hide messagecontext)').should('be.visible');
+    cy.get('[data-cy-open-messagecontext-table]').should('be.checked');
     cy.get('[data-cy-messagecontext-table="table"]').should('be.visible');
     cy.get('[data-cy-messagecontext-table="value"]').should('be.visible');
   });
