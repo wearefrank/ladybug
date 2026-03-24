a72eb5f2128c830e7e065cce57ec12a726897c18
FrancesTwisk <frances.twisk@gmail.com>
2026-03-24 10:18:33 +0100

    Process OTel spans not ladbug spans.

--- ladybug-common/src/main/java/org/wearefrank/ladybug/web/common/CollectorApiImpl.java
+++ ladybug-common/src/main/java/org/wearefrank/ladybug/web/common/CollectorApiImpl.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2025 WeAreFrank!
+   Copyright 2026 WeAreFrank!
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -15,16 +15,20 @@
 */
 package org.wearefrank.ladybug.web.common;
 
+import com.google.protobuf.ByteString;
 import lombok.Setter;
+import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.wearefrank.ladybug.Span;
+import io.opentelemetry.proto.trace.v1.Span;
 import org.wearefrank.ladybug.TestTool;
 
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Component
 public class CollectorApiImpl {
@@ -33,29 +37,53 @@ public class CollectorApiImpl {
 	@Autowired
 	private @Setter TestTool testTool;
 
-	public void processSpans(Span[] trace) {
+	public void processSpans(ArrayList<Span> trace) {
+		System.out.println("CALLED PROCESS SPANS");
 		ArrayList<String> parentIds = new ArrayList<>();
 		for (Span span: trace) {
-			if (span.getParentId() != null && !parentIds.contains(span.getParentId())) {
-				parentIds.add(span.getParentId());
+			String parentId = byteStringToHex(span.getParentSpanId());
+			if (!parentId.isEmpty() && !parentIds.contains(parentId)) {
+				parentIds.add(parentId);
 			}
 		}
+
 		ArrayList<String> endpoints = new ArrayList<>();
-		for (int i = trace.length - 1; i >= 0; i--) {
-			if (trace[i].getParentId() == null) {
-				testTool.startpoint(trace[i].getTraceId(), null, trace[i].getName(), trace[i].toHashmap().toString());
-				endpoints.add(trace[i].getName());
+		for (Span span: trace) {
+			System.out.println(byteStringToHex(span.getTraceId()));
+			String parentId = byteStringToHex(span.getParentSpanId());
+
+			if (parentId.isEmpty()) {
+				testTool.startpoint(byteStringToHex(span.getTraceId()), null, span.getName(), toHashMap(span).toString());
+				endpoints.add(span.getName());
 			} else {
-				if (parentIds.contains(trace[i].getId())) {
-					testTool.startpoint(trace[i].getTraceId(), null, trace[i].getName(), trace[i].toHashmap().toString());
-					endpoints.add(trace[i].getName());
+				if (parentIds.contains(parentId)) {
+					testTool.startpoint(byteStringToHex(span.getTraceId()), null, span.getName(), toHashMap(span).toString());
+					endpoints.add(span.getName());
 				} else {
-					testTool.infopoint(trace[i].getTraceId(), null, trace[i].getName(), trace[i].toHashmap().toString());
+					testTool.infopoint(byteStringToHex(span.getTraceId()), null, span.getName(), toHashMap(span).toString());
 				}
 			}
 		}
 		for (int i = endpoints.size() - 1; i >= 0; i--) {
-			testTool.endpoint(trace[0].getTraceId(), null, endpoints.get(i), "Endpoint");
+			testTool.endpoint(byteStringToHex(trace.get(0).getTraceId()), null, endpoints.get(i), "Endpoint");
 		}
+	}
+
+	public HashMap<String, String> toHashMap(Span span) {
+		HashMap<String, String> map = new HashMap<>();
+
+		span.getAllFields().forEach((descriptor, value) -> {
+			if (value instanceof ByteString) {
+				map.put(descriptor.getName(), byteStringToHex((ByteString) value));
+			} else {
+				map.put(descriptor.getName(), value.toString());
+			}
+		});
+
+		return map;
+	}
+
+	public String byteStringToHex(ByteString byteString) {
+		return Hex.encodeHexString(byteString.toByteArray());
 	}
 }
