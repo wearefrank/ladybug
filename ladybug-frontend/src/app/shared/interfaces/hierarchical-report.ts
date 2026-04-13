5c98bd9ec80d8c2662ea24964e78afbd73f0cdb4
Martijn Dirkse <mhdirkse@pm.me>
2026-04-13 13:12:40 +0200

    Work in progress - bean for JSON report response

---
+++ ladybug-frontend/src/app/shared/interfaces/hierarchical-report.ts
@@ -0,0 +1,34 @@
+export interface TreeItem {
+  name: string;
+  children: HierarchicalCheckpoint[] | null;
+}
+
+export interface HierarchicalReport extends TreeItem {
+  name: string;
+  description: string | null;
+  path: string | null;
+  stubStrategy: string;
+  linkMethod: string;
+  transformation: string | null;
+  storageId: number;
+  storageName: string;
+  estimatedMemoryUsage: number;
+  correlationId: string;
+}
+
+export interface HierarchicalCheckpoint extends TreeItem {
+  message: string | null;
+  encoding: string | null;
+  messageContext: string | null;
+  type: number;
+  level: number;
+  stub: number;
+  stubbed: boolean;
+  stubNotFound: string | null;
+  preTruncatedMessageLength: number;
+  typeAsString: string;
+  threadName: string;
+  sourceClassName: string | null;
+  messageClassName: string | null;
+  uid: string;
+}
