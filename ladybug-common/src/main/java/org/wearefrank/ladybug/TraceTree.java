e3d7ae8e4391cf67eddebab348095f02971248ae
FrancesTwisk <frances.twisk@gmail.com>
2026-04-14 14:28:03 +0200

    Fix.

--- ladybug-common/src/main/java/org/wearefrank/ladybug/TraceTree.java
+++ ladybug-common/src/main/java/org/wearefrank/ladybug/TraceTree.java
@@ -43,7 +43,7 @@ public class TraceTree {
     }
 
     private void dfsRecursive(Span span, HashSet<Span> visited) {
-        testTool.startpoint(byteStringToHex(span.getTraceId()), null, span.getName(), toHashMap(span));
+        testTool.startpoint(byteStringToHex(span.getTraceId()), null, span.getName(), String.valueOf(toHashMap(span)));
 
         for (KeyValue keyValue : span.getAttributesList()) {
             AnyValue anyValue = keyValue.getValue();
