f1641bc9e9640ae75d0c936ffe4065a009fd7b76
Martijn Dirkse <mhdirkse@pm.me>
2026-05-13 16:50:56 +0200

    Add Cypress test that selecting view updates table

--- ladybug-frontend/cypress/e2e/no-profile/report/views.cy.ts
+++ ladybug-frontend/cypress/e2e/no-profile/report/views.cy.ts
@@ -9,6 +9,7 @@ describe('Views', () => {
     // The black box view is not the black box view of the FF!
     // but the black box view of ladybug-test-webapp.
     cy.createReportWithInfopoint();
+    cy.createReportInDatabaseStorage();
     cy.initializeApp();
   });
 
@@ -25,17 +26,25 @@ describe('Views', () => {
     // Expect one open report
     cy.checkFileTreeLength(1);
     checkAllCheckpointsAreShown();
-    // For some reason, the wrong view gets selected when you query by name.
-    // If the black box view ever gets another place in the list, check out the new
-    // index and update this constant.
     const indexOfBlackBoxView = 1;
-    cy.get('[data-cy-change-view-dropdown]').find('option:contains(Black box)').invoke('val').should('equal', `${indexOfBlackBoxView}`);
-    cy.get('[data-cy-change-view-dropdown]').select(indexOfBlackBoxView);
+    changeView('Black box', indexOfBlackBoxView);
     // TODO issue https://github.com/wearefrank/ladybug/issues/802
     // When you select the view, it should not be necessary to reopen
     // the report before it has effect.
     cy.clickRowInTable(0);
     checkOnlyCheckpointsOfBlackBoxViewAreShown();
+  })
+
+  it('When view is changed then debug table is updated', () => {
+    const textWhiteBoxRecord: string = 'Hide a checkpoint in blackbox view';
+    const textDatabaseRecord: string = 'Report for database storage';
+    cy.get('[data-cy-change-view-dropdown] :selected').should('contain.text', "White box");
+    cy.getDebugTableRows().should('have.length', 1).find(`:contains(${textWhiteBoxRecord})`);
+    cy.getDebugTableRows().find(`:contains(${textDatabaseRecord})`).should('not.exist');
+    changeView('Database storage', 4);
+    cy.get('[data-cy-change-view-dropdown] :selected').should('contain.text', "Database storage");
+    cy.getDebugTableRows().find(`:contains(${textWhiteBoxRecord})`).should('not.exist');
+    cy.getDebugTableRows().find(`:contains(${textDatabaseRecord})`).should('have.length', 1);
   })
 })
 
@@ -55,4 +64,12 @@ function checkOnlyCheckpointsOfBlackBoxViewAreShown() {
 
 function checkTreeNodeWithTextOccurs(text: string, amount: number) {
   cy.get('[data-cy-debug-tree="root"] app-tree-icon').parent().find(`:contains(${text})`).should('have.length', amount);
+}
+
+// For some reason, the wrong view gets selected when you query by name.
+// If the view ever gets another place in the list, check out the new
+// index and update the expectedAtIndex passed to this method.
+function changeView(name: string, expectedAtIndex: number) {
+  cy.get('[data-cy-change-view-dropdown]').find(`option:contains(${name})`).invoke('val').should('equal', `${expectedAtIndex}`);
+  cy.get('[data-cy-change-view-dropdown]').select(expectedAtIndex);
 }
