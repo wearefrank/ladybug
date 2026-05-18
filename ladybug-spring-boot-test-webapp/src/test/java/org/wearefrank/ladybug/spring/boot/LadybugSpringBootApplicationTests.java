d5960742320318656da757b6a95bb1537185d889
Martijn Dirkse <mhdirkse@pm.me>
2026-05-18 14:46:40 +0200

    ladybug-spring-boot-test-webapp builds

--- ladybug-spring-boot-test-webapp/src/test/java/org/wearefrank/ladybug/spring/boot/LadybugSpringBootApplicationTests.java
+++ ladybug-spring-boot-test-webapp/src/test/java/org/wearefrank/ladybug/spring/boot/LadybugSpringBootApplicationTests.java
@@ -3,7 +3,7 @@ package org.wearefrank.ladybug.spring.boot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
-class LadybugSpringBootApplicationTests {
+public class LadybugSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
