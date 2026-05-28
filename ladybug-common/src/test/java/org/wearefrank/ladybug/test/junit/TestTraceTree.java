a5c01cc3f08f2a1c52832cdc98a24e6861d7f99c
FrancesTwisk <frances.twisk@gmail.com>
2026-05-28 12:56:02 +0200

    Fix.

--- ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/TestTraceTree.java
+++ ladybug-common/src/test/java/org/wearefrank/ladybug/test/junit/TestTraceTree.java
@@ -4,91 +4,115 @@ import com.google.protobuf.ByteString;
 import io.opentelemetry.proto.common.v1.AnyValue;
 import io.opentelemetry.proto.common.v1.KeyValue;
 import io.opentelemetry.proto.trace.v1.Span;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.wearefrank.ladybug.TestTool;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.wearefrank.ladybug.Report;
 import org.wearefrank.ladybug.TraceTree;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import java.util.List;
 
-class TestTraceTree {
-    private TestTool testTool;
-    private TraceTree traceTree;
+import static org.junit.Assert.*;
 
-    @BeforeEach
-    void setUp() {
-        testTool = Mockito.mock(TestTool.class);
-        traceTree = new TraceTree(testTool);
+public class TestTraceTree extends ReportRelatedTestCase {
+    @Before
+    public void setUp() {
+        super.setUp();
     }
 
     @Test
-    void testDfsOnlyRoot() {
+    public void testDfsOnlyRoot() throws Exception {
         Span span = Span.newBuilder()
                 .setTraceId(ByteString.copyFromUtf8("trace1"))
                 .setSpanId(ByteString.copyFromUtf8("span1"))
                 .setName("root")
                 .addAttributes(KeyValue.newBuilder()
                         .setKey("http.method")
-                        .setValue(AnyValue.newBuilder().setStringValue("GET").build())
+                        .setValue(AnyValue.newBuilder()
+                                .setStringValue("GET")
+                                .build())
                         .build())
                 .build();
 
-        traceTree.dfs(span);
+        TraceTree traceTree = new TraceTree(span, testTool);
 
-        verify(testTool).startpoint(anyString(), isNull(), eq("root"), any());
-        verify(testTool).infopoint(anyString(), isNull(), eq("http.method"), anyString());
-        verify(testTool).endpoint(anyString(), isNull(), eq("root"), eq("Endpoint"));
+        traceTree.dfs();
+
+        String correlationId = traceTree.byteStringToHex(span.getTraceId());
+
+        List<Report> reports = findAndGetReports(
+                testTool,
+                testTool.getDebugStorage(),
+                correlationId,
+                false
+        );
+
+        assertFalse(reports.isEmpty());
+
+        Report report = reports.get(0);
+
+        assertNotNull(report);
+        assertEquals("root", report.getName());
+        assertEquals(3, report.getCheckpoints().size());
     }
 
     @Test
-    void testDfsWithChildSpan() {
+    public void testDfsWithChildSpan() throws Exception {
         Span parent = Span.newBuilder()
-                .setTraceId(ByteString.copyFromUtf8("trace1"))
+                .setTraceId(ByteString.copyFromUtf8("trace2"))
                 .setSpanId(ByteString.copyFromUtf8("parent"))
                 .setName("parent")
                 .build();
 
         Span child = Span.newBuilder()
-                .setTraceId(ByteString.copyFromUtf8("trace1"))
+                .setTraceId(ByteString.copyFromUtf8("trace2"))
                 .setSpanId(ByteString.copyFromUtf8("child"))
                 .setParentSpanId(ByteString.copyFromUtf8("parent"))
                 .setName("child")
                 .build();
 
-        traceTree.addEdge(traceTree.byteStringToHex(parent.getSpanId()), child);
+        TraceTree traceTree = new TraceTree(parent, testTool);
 
-        traceTree.dfs(parent);
+        traceTree.addEdge(
+                traceTree.byteStringToHex(parent.getSpanId()),
+                child
+        );
 
-        verify(testTool).startpoint(anyString(), isNull(), eq("parent"), any());
-        verify(testTool).startpoint(anyString(), isNull(), eq("child"), any());
-        verify(testTool).endpoint(anyString(), isNull(), eq("child"), eq("Endpoint"));
-        verify(testTool).endpoint(anyString(), isNull(), eq("parent"), eq("Endpoint"));
+        traceTree.dfs();
+
+        String correlationId = traceTree.byteStringToHex(parent.getTraceId());
+
+        List<Report> reports = findAndGetReports(
+                testTool,
+                testTool.getDebugStorage(),
+                correlationId,
+                false
+        );
+
+        assertFalse(reports.isEmpty());
+
+        Report report = reports.get(0);
+
+        assertNotNull(report);
+        assertEquals("parent", report.getName());
+        assertTrue(report.getNumberOfCheckpoints() >= 4);
     }
 
     @Test
-    void testByteStringToHex() {
-        ByteString bytes = ByteString.copyFromUtf8("abc");
-        String hex = traceTree.byteStringToHex(bytes);
-
-        assert hex != null;
-        assert !hex.isEmpty();
-        assert hex.equals("616263");
-    }
-
-    @Test
-    void testToHashMap() {
+    public void testToHashMap() {
         Span span = Span.newBuilder()
                 .setTraceId(ByteString.copyFromUtf8("trace1"))
                 .setSpanId(ByteString.copyFromUtf8("span1"))
                 .setName("testSpan")
                 .build();
 
+        TraceTree traceTree = new TraceTree(span, testTool);
+
         var map = traceTree.toHashMap(span);
 
-        assert map.containsKey("trace_id");
-        assert map.containsKey("span_id");
-        assert map.containsKey("name");
+        assertTrue(map.containsKey("trace_id"));
+        assertTrue(map.containsKey("span_id"));
+        assertTrue(map.containsKey("name"));
     }
 }
