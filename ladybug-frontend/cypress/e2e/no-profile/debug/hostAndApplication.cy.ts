5c6775f773f279bcd292e10f138f46c7cb310b22
Martijn Dirkse <mhdirkse@pm.me>
2026-06-16 16:04:23 +0200

    Update Cypress tests

--- ladybug-frontend/cypress/e2e/no-profile/debug/hostAndApplication.cy.ts
+++ ladybug-frontend/cypress/e2e/no-profile/debug/hostAndApplication.cy.ts
@@ -4,30 +4,51 @@ describe('Tests about host and application', () => {
     cy.initializeApp();
   });
 
-  beforeEach(() => {
-    cy.setHostA();
-    cy.setApplicationX();
-    cy.createReport();
-  })
-
   afterEach(() => {
     cy.clearDebugStore();
   });
 
-  it('When default Ladybug names are applied then host and application do not appear in debug table', () => {
+  it('When host and application not set on TestTool then host and application do not appear in debug table', () => {
+    cy.createReport();
     cy.visit('');
     cy.get('[data-cy-debug="refresh"]').click();
     cy.checkDebugTableRowsAre(['Simple report']);
     cy.checkNoHostAndNoApplication(0);
   })
 
-  it('Report metadata holds host and application', () => {
-    cy.visit('');
-    cy.get('[data-cy-debug="refresh"]').click();
-    cy.clickRowInTable(0);
-    cy.clickRootNodeInFileTree();
-    cy.get('[data-cy-open-metadata-table]').check();
-    cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Host A');
-    cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Application X');
+  describe('When host and application are set on TestTool', () => {
+    beforeEach(() => {
+      cy.setHostA();
+      cy.setApplicationX();
+      cy.createReport();
+      cy.setHostB();
+      cy.setApplicationY();
+      cy.createOtherReport();
+    })
+
+    it('Then debug table rows have host and application', () => {
+      cy.visit('');
+      cy.get('[data-cy-debug="refresh"]').click();
+      cy.checkDebugTableRowsAre(['Simple report', 'Another simple report']);
+      cy.checkHostOfDebugTableRow(0, 'Host A');
+      cy.checkApplicationOfDebugTableRow(0, 'Application X');
+      cy.checkHostOfDebugTableRow(1, 'Host B');
+      cy.checkApplicationOfDebugTableRow(1, 'Application Y');
+    })
+
+    it('Then opened report holds host and application', () => {
+      cy.visit('');
+      cy.get('[data-cy-debug="refresh"]').click();
+      cy.clickRowInTable(0);
+      cy.clickRootNodeInFileTree();
+      cy.get('[data-cy-open-metadata-table]').check();
+      cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Host A');
+      cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Application X');
+      cy.clickRowInTable(1);
+      cy.clickRootNodeInFileTree();
+      cy.get('[data-cy-open-metadata-table]').check();
+      cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Host B');
+      cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Application Y');
+    })
   })
 })
