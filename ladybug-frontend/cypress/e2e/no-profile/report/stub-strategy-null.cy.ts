824bd75c66801ec790464eecec3824294e3c8034
Martijn Dirkse <mhdirkse@pm.me>
2026-06-19 11:45:47 +0200

    Extend previous commit

--- ladybug-frontend/cypress/e2e/no-profile/report/stub-strategy-null.cy.ts
+++ ladybug-frontend/cypress/e2e/no-profile/report/stub-strategy-null.cy.ts
@@ -40,4 +40,30 @@ describe('Stub strategy null', () => {
     cy.get('[data-cy-difference-modal="confirm"]').click();
     cy.get('[data-cy-report-stub-strategy]').find(':selected').invoke('text').should('equal', 'Always');
   })
+
+  it('When other checkpoint item is edited then stub strategy remains unselected', () => {
+    cy.checkTestTableReportsAre(['Report without stub strategy and without link method']);
+    cy.get('[data-cy-test="openReport"]').click();
+    cy.editCheckpointValue('Edited value');
+    cy.get('[data-cy-report-stub-strategy]').find(':selected').should('not.exist');
+    cy.get('[data-cy-report="save"]').click();
+    cy.get('[data-cy-difference-modal="confirm"]').click();
+    cy.get('[data-cy-element-name="checkpointEditor"]')
+      .should('contain.text', 'Edited')
+      .should('contain.text', 'value');
+    cy.get('[data-cy-report-stub-strategy]').find(':selected').should('not.exist');
+  })
+
+  it('When other report item is edited then stub strategy remains unselected', () => {
+    cy.checkTestTableReportsAre(['Report without stub strategy and without link method']);
+    cy.get('[data-cy-test="openReport"]').click();
+    cy.clickRootNodeInFileTree();
+    cy.get('[data-cy-element-name="description"]').clear().type('Edited description');
+    cy.get('[data-cy-report-stub-strategy]').find(':selected').should('not.exist');
+    cy.get('[data-cy-report="save"]').click();
+    cy.get('[data-cy-difference-modal="confirm"]').click();
+    cy.clickRootNodeInFileTree();
+    cy.get('[data-cy-element-name="description"]').invoke('val').should('contain', 'Edited description');
+    cy.get('[data-cy-report-stub-strategy]').find(':selected').should('not.exist');
+  })
 })
