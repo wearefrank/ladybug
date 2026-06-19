8a3b3438e21f51c38c6f3c4c6f471d4793531605
Martijn Dirkse <mhdirkse@pm.me>
2026-06-19 14:37:54 +0200

    Cover copying reports with host and application

--- ladybug-frontend/cypress/e2e/no-profile/debug/hostAndApplication.cy.ts
+++ ladybug-frontend/cypress/e2e/no-profile/debug/hostAndApplication.cy.ts
@@ -1,3 +1,5 @@
+import { STORAGE_ID_COLUMN } from '../../../support/e2e';
+
 describe('Tests about host and application', () => {
   before(() => {
     cy.resetApp();
@@ -53,6 +55,33 @@ describe('Tests about host and application', () => {
       cy.get('[data-cy-open-metadata-table]').check();
       cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Host B');
       cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Application Y');
+    })
+
+    it('Then host and application are still set when report is copied', () => {
+      cy.visit('');
+      cy.get('[data-cy-debug="refresh"]').click();
+      cy.clickRowInTable(0);
+      cy.debugTreeGuardedCopyReport('Simple report', 3, '');
+      cy.navigateToTestTabAndAwaitLoadingSpinner();
+      cy.get('[data-cy-test="showHideStorageIds"]').should('contain.text', 'Show').click();
+      cy.get('[data-cy-test="showHideStorageIds"]').should('contain.text', 'Hide');
+      cy.get('[data-cy-test="tableRow"]')
+        .should('have.length', 1)
+        .find('td').eq(STORAGE_ID_COLUMN)
+        .should('have.text', '0');
+      cy.get('[data-cy-test="tableRow"]').find('td').eq(0).find('input').check();
+      cy.get('[data-cy-test="copy"]').click();
+      cy.get('[data-cy-test="tableRow"]').should('have.length', 2)
+        .eq(1)
+        .find('td').eq(STORAGE_ID_COLUMN)
+        .should('have.text', 1);
+      cy.get('[data-cy-test="tableRow"]').eq(1)
+        .find('[data-cy-test="openReport"]')
+        .click();
+      cy.clickRootNodeInFileTree();
+      cy.get('[data-cy-open-metadata-table]').check();
+      cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Host A');
+      cy.get('[data-cy-metadata-table="table"]').should('contain.text', 'Application X');      
     })
   })
 })
