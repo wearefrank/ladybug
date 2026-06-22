5c67bca55242076d9867d846122eb29d41f3509d
Martijn Dirkse <mhdirkse@pm.me>
2026-06-22 10:42:45 +0200

    Refactor SortableTable

--- ladybug-frontend/src/app/debug/sortable-table/sortable-table.ts
+++ ladybug-frontend/src/app/debug/sortable-table/sortable-table.ts
@@ -17,9 +17,21 @@ export interface Column {
   label: string;
 }
 
-export interface RowData {
+export interface TableData {
+  rows: Record<string, string>[];
+  columns: Column[];
+  numericMetadataNames: Set<string>;
+}
+
+interface RowData {
   checked: boolean;
   fields: Record<string, string>;
+}
+
+interface WorkingData {
+  rows: RowData[];
+  columns: Column[];
+  numericMetadataNames: Set<string>;
 }
 
 @Component({
@@ -38,20 +50,32 @@ export interface RowData {
   ],
 })
 export class SortableTable implements OnInit {
-  @Input() set data(argument: Record<string, string>[]) {
-    this.rows = argument.map((d) => {
-      return { checked: false, fields: d };
-    });
+  @Input() set tableData(argument: TableData) {
+    this.data = {
+      rows: argument.rows.map((r) => {
+        return { checked: false, fields: r };
+      }),
+      columns: argument.columns,
+      numericMetadataNames: argument.numericMetadataNames,
+    };
+    const storageIdColumns: Column[] = this.data.columns.filter((c) => c.name === STORAGE_ID_COLUMN_NAME);
+    if (storageIdColumns.length !== 1) {
+      throw new Error(`SortableTable.set tableData(): Expected column ${STORAGE_ID_COLUMN_NAME}`);
+    }
+    if (!this.data.numericMetadataNames.has(STORAGE_ID_COLUMN_NAME)) {
+      throw new Error(`SortableTableData.set tableData(): Expected column ${STORAGE_ID_COLUMN_NAME} to be numeric`);
+    }
+    const statusColumns: Column[] = this.data.columns.filter((c) => c.name === STATUS_COLUMN_NAME);
+    if (statusColumns.length !== 1) {
+      throw new Error(`SortableTableData.set tableData(): Expected column ${STATUS_COLUMN_NAME}`);
+    }
   }
-
-  @Input() columns?: Column[];
-  @Input() numericMetadataNames?: Set<string>;
   @Input() selectedStorageId: string | null = null;
   @Output() clickReportWithStorageId: EventEmitter<number> = new EventEmitter<number>();
 
+  protected data?: WorkingData;
   protected tableDataSort?: MatSort;
   protected tableDataSource: MatTableDataSource<RowData> = new MatTableDataSource<RowData>();
-  protected rows?: RowData[];
   protected checkboxSize?: string;
   protected fontSize?: string;
   protected tableSpacing?: string;
@@ -63,16 +87,12 @@ export class SortableTable implements OnInit {
   @ViewChild(MatSort)
   protected set matSort(sort: MatSort) {
     this.tableDataSort = sort;
-    // TODO: Get rid of !
     this.tableDataSource.sort = this.tableDataSort;
     this.tableDataSource.sortingDataAccessor = (row: RowData, header: string): string | number =>
       this.sortingDataAccessor(row, header);
   }
 
   ngOnInit(): void {
-    if (!this.numericMetadataNames?.has(STORAGE_ID_COLUMN_NAME)) {
-      throw new Error('Expected that column storageId is numeric');
-    }
     this.subscribeToObservables();
   }
 
@@ -89,46 +109,54 @@ export class SortableTable implements OnInit {
   }
 
   protected allChecked(): boolean {
-    this.checkInitialized();
-    if (this.rows!.length === 0) {
-      return false;
-    }
-    for (const row of this.rows!) {
-      if (row.checked === false) {
+    if (this.data) {
+      if (this.data.rows.length === 0) {
         return false;
       }
+      for (const row of this.data.rows) {
+        if (row.checked === false) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      // Dummy return value
+      return true;
     }
-    return true;
   }
 
   protected noneChecked(): boolean {
-    this.checkInitialized();
-    if (this.rows!.length === 0) {
-      return false;
-    }
-    for (const row of this.rows!) {
-      if (row.checked === true) {
+    if (this.data) {
+      if (this.data.rows.length === 0) {
         return false;
       }
+      for (const row of this.data.rows) {
+        if (row.checked === true) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+      // Dummy return value
+      return true;
     }
-    return true;
   }
 
   protected selectAll(value: boolean): void {
-    for (const row of this.rows!) {
-      row.checked = value;
+    if (this.data) {
+      for (const row of this.data.rows) {
+        row.checked = value;
+      }
     }
   }
 
   protected toggleSelectAll(): void {
-    this.checkInitialized();
-    if (this.rows!.length === 0) {
-      return;
-    }
-    if (this.noneChecked()) {
-      this.selectAll(true);
-    } else {
-      this.selectAll(false);
+    if (this.data && this.data.rows.length > 0) {
+      if (this.noneChecked()) {
+        this.selectAll(true);
+      } else {
+        this.selectAll(false);
+      }
     }
   }
 
@@ -137,18 +165,15 @@ export class SortableTable implements OnInit {
   }
 
   protected getColumnNames(): string[] {
-    this.checkInitialized();
-    return this.columns!.map((c) => c.name);
+    return this.data?.columns.map((c) => c.name) ?? [];
   }
 
   protected getColumnLabels(): string[] {
-    this.checkInitialized();
-    return this.columns!.map((c) => c.label);
+    return this.data?.columns.map((c) => c.label) ?? [];
   }
 
   sortingDataAccessor(row: RowData, columnName: string): string | number {
-    this.checkInitialized();
-    if (this.numericMetadataNames!.has(columnName)) {
+    if (this.data && this.data.numericMetadataNames.has(columnName)) {
       const result: number = +row.fields[columnName];
       return result;
     } else {
@@ -157,7 +182,7 @@ export class SortableTable implements OnInit {
   }
 
   protected getMetadata(row: RowData, name: string): string {
-    return row.fields[name];
+    return row.fields[name] ?? '';
   }
 
   protected getStatusClass(row: RowData): string {
@@ -187,15 +212,5 @@ export class SortableTable implements OnInit {
 
   private setTableSpacing(value: number): void {
     this.tableSpacing = `${value * 0.25}em 0 ${value * 0.25}em 0`;
-  }
-
-  private checkInitialized(): void {
-    if (!this.isInitialized()) {
-      throw new Error('Expected data of SortableTable to be initialized');
-    }
-  }
-
-  private isInitialized(): boolean {
-    return this.data !== undefined && this.columns !== undefined && this.numericMetadataNames !== undefined;
   }
 }
