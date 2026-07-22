6670eba44b604a0f9b3339ce6d87f9320c1ed4bf
Martijn Dirkse <mhdirkse@pm.me>
2026-07-22 19:28:40 +0200

    Fix ladybug-ff-cypress-test (#878)
    * Fix ladybug-ff-cypress-test
    
    * End time --> End Time
    
    * Make debug table headers equal to side drawer headers
    
    * Shorten filter sphere labels
    
    * Base filter key shown in filter spheres on column label instead of column name
    
    * Column labels are equal in debug table, filter drawer and spherical filter labels

--- ladybug-frontend/src/app/debug/active-filters/active-filters.component.ts
+++ ladybug-frontend/src/app/debug/active-filters/active-filters.component.ts
@@ -4,33 +4,54 @@ import { NgClass, TitleCasePipe } from '@angular/common';
 import { catchError, Subscription } from 'rxjs';
 import { ErrorHandling } from '../../shared/classes/error-handling.service';
 import { FilterService } from '../../shared/services/filter.service';
+import { ShortenedTableHeaderPipe } from '../../shared/pipes/shortened-table-header.pipe';
 
 @Component({
   selector: 'app-active-filters',
   templateUrl: './active-filters.component.html',
   styleUrls: ['./active-filters.component.css'],
   standalone: true,
-  imports: [NgClass, TitleCasePipe, DictionaryPipe],
+  imports: [NgClass, TitleCasePipe, ShortenedTableHeaderPipe, DictionaryPipe],
 })
 export class ActiveFiltersComponent implements OnInit, OnDestroy {
+  private columnNameToLabel: Map<string, string> = new Map<string, string>();
   protected activeFilters: Map<string, string> = new Map<string, string>();
-  private filterContextSubscription: Subscription = new Subscription();
+  private subscriptions: Subscription = new Subscription();
 
   private filterService = inject(FilterService);
   private errorHandler = inject(ErrorHandling);
 
   ngOnInit(): void {
-    this.filterContextSubscription = this.filterService.userFilters$.subscribe({
-      next: (context: Map<string, string>) => this.changeFilter(context),
-      error: () => catchError(this.errorHandler.handleError()),
-    });
+    this.subscriptions.add(
+      this.filterService.userFilterColumns$.subscribe((columns) => {
+        if (!columns) {
+          return;
+        }
+        this.columnNameToLabel.clear();
+        for (const column of columns) {
+          this.columnNameToLabel.set(column.name, column.label);
+        }
+      }),
+    );
+    this.subscriptions.add(
+      this.filterService.userFilters$.subscribe({
+        next: (context: Map<string, string>) => this.changeFilter(context),
+        error: () => catchError(this.errorHandler.handleError()),
+      }),
+    );
   }
 
   ngOnDestroy(): void {
-    this.filterContextSubscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 
   changeFilter(context: Map<string, string>): void {
+    // Use the column name as key of this map, even though the column label is the base of what is shown.
+    // We only know for sure that the column name is unique.
     this.activeFilters = new Map<string, string>(context);
+  }
+
+  protected safeColumnNameToLabel(columnName: string): string {
+    return this.columnNameToLabel.get(columnName) ?? columnName;
   }
 }
