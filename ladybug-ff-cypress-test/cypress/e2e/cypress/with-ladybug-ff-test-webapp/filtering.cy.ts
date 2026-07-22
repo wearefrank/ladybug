da46b559e73cbabf523564d413aae123ea05d66f
Martijn Dirkse <mhdirkse@pm.me>
2026-07-22 18:55:45 +0200

    Column labels are equal in debug table, filter drawer and spherical filter labels

--- ladybug-ff-cypress-test/cypress/e2e/cypress/with-ladybug-ff-test-webapp/filtering.cy.ts
+++ ladybug-ff-cypress-test/cypress/e2e/cypress/with-ladybug-ff-test-webapp/filtering.cy.ts
@@ -24,7 +24,6 @@ describe('Tests with views and filtering', () => {
   interface ColumnAndName {
     readonly name: string
     readonly colNr: number
-    readonly filterSphereLabel: string
     // TODO: Get rid of this when issue https://github.com/wearefrank/ladybug/issues/429 is fixed
     readonly enabled: boolean
   }
@@ -32,16 +31,16 @@ describe('Tests with views and filtering', () => {
   const isDatabaseStorage: boolean = Cypress.env('debugStorageName') === 'DatabaseDebugStorage'
 
   const columnAndNameCombinations: ColumnAndName[] = [
-    { name: 'Storage Id', colNr: 1, filterSphereLabel: 'Storageid', enabled: !isDatabaseStorage },
-    { name: 'End Time', colNr: 2, filterSphereLabel: 'Endtime', enabled: !isDatabaseStorage },
-    { name: 'Duration', colNr: 3, filterSphereLabel: 'Duration', enabled: true },
-    { name: 'Name', colNr: 4, filterSphereLabel: 'Name', enabled: true },
-    { name: 'Correlation Id', colNr: 5, filterSphereLabel: 'Correlationid', enabled: true },
-    { name: 'Status', colNr: 6, filterSphereLabel: 'Status', enabled: true },
-    { name: 'Checkpoints', colNr: 7, filterSphereLabel: 'Checkpoints', enabled: !isDatabaseStorage },
-    { name: 'Memory', colNr: 8, filterSphereLabel: 'Memory', enabled: true },
-    { name: 'Size', colNr: 9, filterSphereLabel: 'Size', enabled: true },
-    { name: 'Input', colNr: 10, filterSphereLabel: 'Input', enabled: true }
+    { name: 'Storage Id', colNr: 1, enabled: !isDatabaseStorage },
+    { name: 'End Time', colNr: 2, enabled: !isDatabaseStorage },
+    { name: 'Duration', colNr: 3, enabled: true },
+    { name: 'Name', colNr: 4, enabled: true },
+    { name: 'Correlation Id', colNr: 5, enabled: true },
+    { name: 'Status', colNr: 6, enabled: true },
+    { name: 'Checkpoints', colNr: 7, enabled: !isDatabaseStorage },
+    { name: 'Memory', colNr: 8, enabled: true },
+    { name: 'Size', colNr: 9, enabled: true },
+    { name: 'Input', colNr: 10, enabled: true }
   ]
 
   const testedColumnAndNameCombinations = columnAndNameCombinations.filter((testCase) => testCase.name !== 'Status')
@@ -61,12 +60,12 @@ describe('Tests with views and filtering', () => {
         cy.enterFilter(testCase.name, firstRowFieldValue)
         cy.inIframeBody('[data-cy-debug="tableRow"]').should('have.length.lessThan', 5)
         cy.inIframeBody('[data-cy-debug="tableRow"]').should('have.length.greaterThan', 0)
-        cy.checkActiveFilterSphere(testCase.filterSphereLabel, firstRowFieldValue).should('be.visible')
+        cy.checkActiveFilterSphere(testCase.name, firstRowFieldValue).should('be.visible')
         cy.inIframeBody('[data-cy-debug="clear-filter-btn"]').should('have.length', 1).click()
         cy.inIframeBody('[data-cy-debug="tableRow"]').should('have.length', 5)
         cy.inIframeBody('[data-cy-debug="close-filter-btn"]').click()
         cy.inIframeBody('[data-cy-debug="close-filter-btn"]').should('not.exist')
-        cy.checkActiveFilterSphere(testCase.filterSphereLabel, firstRowFieldValue).should('not.exist')
+        cy.checkActiveFilterSphere(testCase.name, firstRowFieldValue).should('not.exist')
       })
     })
   }
