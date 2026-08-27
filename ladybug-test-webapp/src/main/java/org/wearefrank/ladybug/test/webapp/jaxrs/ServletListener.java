5cf270a3cff76bef28dd12547a1b6ff5f3d6428e
Martijn Dirkse <martijn@wearefrank.nl>
2026-08-27 10:42:29 +0200

    Update package name of JAXRS test webapp

--- ladybug-test-webapp/src/main/java/org/wearefrank/ladybug/test/webapp/jaxrs/ServletListener.java
+++ ladybug-test-webapp/src/main/java/org/wearefrank/ladybug/test/webapp/jaxrs/ServletListener.java
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-package org.wearefrank.ladybug.test.web;
+package org.wearefrank.ladybug.test.webapp.jaxrs;
 
 
 import java.lang.invoke.MethodHandles;
